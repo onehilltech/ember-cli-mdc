821d2ff44c0ef464ff64053c5a52ecfa222223e8
James H. Hill <hilljh82@gmail.com>
2018-08-03 18:25:30 -0400

    Removed commented out code

--- packages/mdc-theme/blueprints/ember-cli-mdc-theme/index.js
+++ packages/mdc-theme/blueprints/ember-cli-mdc-theme/index.js
@@ -4,13 +4,6 @@ module.exports = {
 
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
-  // locals(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
   afterInstall () {
     return this.addPackagesToProject ([
       {name: '@material/theme'},
