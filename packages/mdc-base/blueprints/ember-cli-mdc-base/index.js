a5625afcb3dbf30e0f5d9a78e24db1e4d1a5539d
James H. Hill <hilljh82@gmail.com>
2018-08-03 21:20:03 -0400

    Fixed blueprint

--- packages/mdc-base/blueprints/ember-cli-mdc-base/index.js
+++ packages/mdc-base/blueprints/ember-cli-mdc-base/index.js
@@ -2,6 +2,8 @@
 module.exports = {
   description: '',
 
+  normalizeEntityName() {}, // no-op since we're just adding dependencies
+
   afterInstall () {
     return this.addPackagesToProject ([
       {name: '@material/base'},
