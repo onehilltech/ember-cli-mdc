0b75f33d10403811badb92f4471930d40e6733fa
James H. Hill <hilljh82@gmail.com>
2018-08-04 14:42:09 -0400

    Updating documentation for using theme; and exemplified it

--- packages/mdc-button/ember-cli-build.js
+++ packages/mdc-button/ember-cli-build.js
@@ -1,15 +1,21 @@
 'use strict';
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const path = require ('path');
 
 module.exports = function(defaults) {
-  let app = new EmberAddon(defaults, {
+  const options = {
     sassOptions: {
       includePaths: [
-        './node_modules'
+        path.resolve ('./node_modules')
       ]
     }
-  });
+  };
+
+  if (defaults.project.pkg.name === 'ember-cli-mdc-button')
+    options.sassOptions.includePaths.push (path.resolve ('./tests/dummy/app/styles'));
+
+  let app = new EmberAddon (defaults, options);
 
   /*
     This build file specifies the options for the dummy test app of this
