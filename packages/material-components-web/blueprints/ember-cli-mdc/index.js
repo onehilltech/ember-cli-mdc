55e94ca0801dc423d8ec805aa0ffaa27da9d36b8
James H. Hill <hilljh82@gmail.com>
2018-08-12 23:36:09 -0400

    Added wrapper package

---
+++ packages/material-components-web/blueprints/ember-cli-mdc/index.js
@@ -0,0 +1,18 @@
+/* eslint-env node */
+module.exports = {
+  description: '',
+
+  normalizeEntityName() {}, // no-op since we're just adding dependencies
+
+  afterInstall () {
+    return this.addAddonsToProject ({
+      packages: [
+        {name: 'ember-cli-mdc-button'},
+        {name: 'ember-cli-mdc-card'},
+        {name: 'ember-cli-mdc-fab'},
+        {name: 'ember-cli-mdc-form'},
+        {name: 'ember-cli-mdc-snackbar'}
+      ]
+    });
+  }
+};
