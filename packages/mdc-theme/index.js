1c2a16a6c5c1c829e19b6a451ab26a1ec75c41fe
James H. Hill <hilljh82@gmail.com>
2018-08-17 00:31:24 -0400

    Added theme color support to the html window

--- packages/mdc-theme/index.js
+++ packages/mdc-theme/index.js
@@ -1,5 +1,19 @@
 'use strict';
 
+const { get } = require ('lodash');
+
 module.exports = {
   name: 'ember-cli-mdc-theme',
+
+  contentFor (type, config) {
+    if (type === 'head') {
+      let themeColor = get (config, 'ember-cli-mdc.themeColor');
+
+      if (themeColor) {
+        return `<meta name="theme-color" content="${themeColor}" />
+               <meta name="msapplication-navbutton-color" content="${themeColor}" />
+               <meta name="apple-mobile-web-app-status-bar-style" content="${themeColor}" />`;
+      }
+    }
+  }
 };
