8fbce51421d7a4a5252ad65479a2db342ecb3c4d
James H. Hill <hilljh82@gmail.com>
2018-08-17 19:53:19 -0400

    Separate the theme into mixin for text, background, and surface

--- packages/mdc-theme/addon/index.js
+++ packages/mdc-theme/addon/index.js
@@ -1,3 +1,11 @@
 import Theme from './mixins/theme';
+import TextTheme from './mixins/text-theme';
+import BackgroundTheme from './mixins/background-theme';
+import SurfaceTheme from './mixins/surface-theme';
 
-export { Theme };
+export {
+  Theme,
+  TextTheme,
+  BackgroundTheme,
+  SurfaceTheme
+};
