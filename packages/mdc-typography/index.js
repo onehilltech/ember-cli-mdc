08d5a681510f67c6eaae432d4525b78e6544a25c
James H. Hill <hilljh82@gmail.com>
2018-08-17 09:40:43 -0400

    Added support for CORBER by embedding Roboto fonts in application

--- packages/mdc-typography/index.js
+++ packages/mdc-typography/index.js
@@ -3,6 +3,27 @@
 module.exports = {
   name: 'ember-cli-mdc-typography',
 
+  included (app) {
+    this._super (...arguments);
+
+    if (process.env.CORBER) {
+      // Import the font stylesheet.
+      app.import ('vendor/assets/styles/roboto.css');
+
+      // Import the Roboto fonts.
+      app.import ('vendor/assets/fonts/roboto/Roboto-Bold.woff', {destDir: 'assets/fonts'});
+      app.import ('vendor/assets/fonts/roboto/Roboto-Bold.woff2', {destDir: 'assets/fonts'});
+      app.import ('vendor/assets/fonts/roboto/Roboto-Light.woff', {destDir: 'assets/fonts'});
+      app.import ('vendor/assets/fonts/roboto/Roboto-Light.woff2', {destDir: 'assets/fonts'});
+      app.import ('vendor/assets/fonts/roboto/Roboto-Medium.woff', {destDir: 'assets/fonts'});
+      app.import ('vendor/assets/fonts/roboto/Roboto-Medium.woff2', {destDir: 'assets/fonts'});
+      app.import ('vendor/assets/fonts/roboto/Roboto-Regular.woff', {destDir: 'assets/fonts'});
+      app.import ('vendor/assets/fonts/roboto/Roboto-Regular.woff2', {destDir: 'assets/fonts'});
+      app.import ('vendor/assets/fonts/roboto/Roboto-Thin.woff', {destDir: 'assets/fonts'});
+      app.import ('vendor/assets/fonts/roboto/Roboto-Thin.woff2', {destDir: 'assets/fonts'});
+    }
+  },
+
   contentFor (type, config) {
     this._super (...arguments);
 
