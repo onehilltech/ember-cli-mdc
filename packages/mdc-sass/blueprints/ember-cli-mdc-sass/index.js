5d059a36163230650e8da5b526523a0d5ffa059c
James H. Hill <hilljh82@gmail.com>
2018-08-17 22:01:21 -0400

    Added missing blueprints

---
+++ packages/mdc-sass/blueprints/ember-cli-mdc-sass/index.js
@@ -0,0 +1,14 @@
+/* eslint-env node */
+module.exports = {
+  description: '',
+
+  normalizeEntityName() {}, // no-op since we're just adding dependencies
+
+  afterInstall () {
+    return this.addAddonsToProject ({
+      packages: [
+        {name: 'ember-cli-mdc-sass'}
+      ]
+    })
+  }
+};
