072e768c99571572f97eab3ffae91c84a23b3cda
James H. Hill <hilljh82@gmail.com>
2018-08-18 02:47:19 -0400

    isAddonProject failed if package missing keywords

--- packages/mdc-sass/blueprints/ember-cli-mdc-sass/index.js
+++ packages/mdc-sass/blueprints/ember-cli-mdc-sass/index.js
@@ -1,8 +1,46 @@
 /* eslint-env node */
+
+const path = require ('path');
+const fs = require ('fs');
+
+Array.prototype.remove = function (e) {
+  const index = this.indexOf (e);
+
+  if (index >= 0)
+    this.splice (index, 1);
+
+  return this;
+};
+
 module.exports = {
   description: '',
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
+  normalizeEntityName () {
+    // no-op since we're just adding dependencies
+  },
+
+  files () {
+    const files = this._super (...arguments);
+
+    if (this.isAddonProject ()) {
+      // Remove the files that to not pertain to add-ons.
+      files.remove ('__root__/styles/app.scss');
+
+      // We do not need to continue overwrite the theme file.
+      if (this.name === 'ember-cli-mdc-sass' || this.fileExistsSync ('addon/styles/addon.scss'))
+        files.remove ('__root__/styles/addon.scss');
+    }
+    else {
+      // Remove files that do not pertain to applications projects.
+      files.remove ('__root__/styles/addon.scss');
+
+      // Only include the theme file if it does not already exist.
+      if (this.fileExistsSync ('app/styles/app.scss'))
+        files.remove ('__root__/styles/app.scss');
+    }
+
+    return files;
+  },
 
   afterInstall () {
     return this.addAddonsToProject ({
@@ -14,5 +52,13 @@ module.exports = {
         save: true
       }
     });
+  },
+
+  isAddonProject () {
+    return this.options.project.pkg.keywords && this.options.project.pkg.keywords.includes ('ember-addon');
+  },
+
+  fileExistsSync (file) {
+    return fs.existsSync (path.resolve (this.options.project.root, file));
   }
 };
--- packages/mdc-theme/blueprints/ember-cli-mdc-theme/index.js
+++ packages/mdc-theme/blueprints/ember-cli-mdc-theme/index.js
@@ -1,8 +1,44 @@
 /* eslint-env node */
+
+const path = require ('path');
+const fs = require ('fs');
+
+Array.prototype.remove = function (e) {
+  const index = this.indexOf (e);
+
+  if (index >= 0)
+    this.splice (index, 1);
+
+  return this;
+};
+
 module.exports = {
   description: '',
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
+  normalizeEntityName () {
+    // no-op since we're just adding dependencies
+  },
+
+  files () {
+    const files = this._super (...arguments);
+
+    if (this.isAddonProject ()) {
+      // Remove the files that to not pertain to add-ons.
+      files.remove ('__root__/styles/_app-theme.scss');
+
+      if (this.fileExistsSync ('tests/dummy/app/styles/_app-theme.scss'))
+        files.remove ('tests/dummy/app/styles/_app-theme.scss');
+    }
+    else {
+      // Remove files that do not pertain to applications projects.
+      files.remove ('tests/dummy/app/styles/_app-theme.scss');
+
+      if (this.fileExistsSync ('app/styles/_app-theme.scss'))
+        files.remove ('__root__/styles/_app-theme.scss');
+    }
+
+    return files;
+  },
 
   afterInstall () {
     return this.addAddonsToProject ({
@@ -14,5 +50,13 @@ module.exports = {
         {name: '@material/theme'},
       ]);
     });
+  },
+
+  isAddonProject () {
+    return this.options.project.pkg.keywords && this.options.project.pkg.keywords.includes ('ember-addon');
+  },
+
+  fileExistsSync (file) {
+    return fs.existsSync (path.resolve (this.options.project.root, file));
   }
 };
