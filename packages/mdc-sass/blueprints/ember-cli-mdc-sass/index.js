abf4317e7188df9d3df036da51e31a689a8c4a21
James H. Hill <hilljh82@gmail.com>
2018-08-18 09:38:41 -0400

    Added missing files; updated blueprint to use EmberCli API correctly

--- packages/mdc-sass/blueprints/ember-cli-mdc-sass/index.js
+++ packages/mdc-sass/blueprints/ember-cli-mdc-sass/index.js
@@ -1,17 +1,5 @@
 /* eslint-env node */
 
-const path = require ('path');
-const fs = require ('fs');
-
-Array.prototype.remove = function (e) {
-  const index = this.indexOf (e);
-
-  if (index >= 0)
-    this.splice (index, 1);
-
-  return this;
-};
-
 module.exports = {
   description: '',
 
@@ -20,23 +8,23 @@ module.exports = {
   },
 
   files () {
-    const files = this._super (...arguments);
+    let files = this._super (...arguments);
 
-    if (this.isAddonProject ()) {
+    if (this.project.isEmberCLIAddon ()) {
       // Remove the files that to not pertain to add-ons.
-      files.remove ('__root__/styles/app.scss');
+      files = files.filter (item => item !== '__root__/styles/app.scss');
 
       // We do not need to continue overwrite the theme file.
-      if (this.name === 'ember-cli-mdc-sass' || this.fileExistsSync ('addon/styles/addon.scss'))
-        files.remove ('__root__/styles/addon.scss');
+      if (this.name === 'ember-cli-mdc-sass' || this.project.has ('addon/styles/addon.scss'))
+        files = files.filter (item => item !== '__root__/styles/addon.scss');
     }
     else {
       // Remove files that do not pertain to applications projects.
-      files.remove ('__root__/styles/addon.scss');
+      files = files.filter (item => item !== '__root__/styles/addon.scss');
 
       // Only include the theme file if it does not already exist.
-      if (this.fileExistsSync ('app/styles/app.scss'))
-        files.remove ('__root__/styles/app.scss');
+      if (this.project.has ('app/styles/app.scss'))
+        files = files.filter (item => item !== '__root__/styles/app.scss');
     }
 
     return files;
@@ -52,13 +40,5 @@ module.exports = {
         save: true
       }
     });
-  },
-
-  isAddonProject () {
-    return this.options.project.pkg.keywords && this.options.project.pkg.keywords.includes ('ember-addon');
-  },
-
-  fileExistsSync (file) {
-    return fs.existsSync (path.resolve (this.options.project.root, file));
   }
 };
