4a5ce5c2acc687be19d7a92b0e6ef13dea4492d2
James H. Hill <hilljh82@gmail.com>
2018-08-18 00:48:15 -0400

    Fixed build

--- packages/mdc-button/ember-cli-build.js
+++ packages/mdc-button/ember-cli-build.js
@@ -4,18 +4,9 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 const path = require ('path');
 
 module.exports = function(defaults) {
-  const options = {
-    sassOptions: {
-      includePaths: [
-        path.resolve ('./node_modules')
-      ]
-    }
-  };
+  let app = new EmberAddon (defaults, {
 
-  if (defaults.project.pkg.name === 'ember-cli-mdc-button')
-    options.sassOptions.includePaths.push (path.resolve ('./tests/dummy/app/styles'));
-
-  let app = new EmberAddon (defaults, options);
+  });
 
   /*
     This build file specifies the options for the dummy test app of this
