7aa0d7ca5a5f86df7eb510efe93c69f9af909530
James H. Hill <hilljh82@gmail.com>
2018-08-18 01:26:50 -0400

    Removed Promise.all from blueprint

--- packages/mdc-animation/blueprints/ember-cli-mdc-animation/index.js
+++ packages/mdc-animation/blueprints/ember-cli-mdc-animation/index.js
@@ -5,16 +5,14 @@ module.exports = {
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall () {
-    return Promise.all ([
-      this.addAddonsToProject ({
+    return this.addPackagesToProject ([
+      {name: '@material/animation'},
+    ]).then (() => {
+      return this.addAddonsToProject ({
         packages: [
           {name: 'ember-cli-mdc-sass'}
         ]
-      }),
-
-      this.addPackagesToProject ([
-        {name: '@material/animation'},
-      ])
-    ]);
+      });
+    });
   }
 };
