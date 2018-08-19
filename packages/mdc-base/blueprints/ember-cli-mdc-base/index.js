8071e13ea3e478518f92f7c697b94e0e9e19be62
James H. Hill <hilljh82@gmail.com>
2018-08-19 01:52:57 -0400

    Updated dependencies

--- packages/mdc-base/blueprints/ember-cli-mdc-base/index.js
+++ packages/mdc-base/blueprints/ember-cli-mdc-base/index.js
@@ -1,18 +1,23 @@
 /* eslint-env node */
+
+const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+
 module.exports = {
   description: '',
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
+  normalizeEntityName () {
+    // no-op since we're just adding dependencies
+  },
 
   afterInstall () {
-    return this.addPackagesToProject ([
-      {name: '@material/base'},
+    return installPackages (this, [
+      {name: '@material/base'}
     ]).then (() => {
-      return this.addAddonsToProject ({
+      return installAddons (this, {
         packages: [
-          {name: 'ember-cli-mdc-sass'},
+          {name: 'ember-cli-mdc-sass'}
         ]
-      })
-    })
+      });
+    });
   }
 };
