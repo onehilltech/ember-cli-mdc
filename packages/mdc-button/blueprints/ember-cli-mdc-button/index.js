11e0ac087ef3d1c61a9415567146c7fd6ee671e7
James H. Hill <hilljh82@gmail.com>
2018-08-19 15:22:38 -0400

    Updated dependencies

--- packages/mdc-button/blueprints/ember-cli-mdc-button/index.js
+++ packages/mdc-button/blueprints/ember-cli-mdc-button/index.js
@@ -1,22 +1,27 @@
 /* eslint-env node */
+
+const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+
 module.exports = {
   description: '',
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
+  normalizeEntityName () {
+    // no-op since we're just adding dependencies
+  },
 
   afterInstall () {
-    return this.addAddonsToProject ({
-      packages: [
-        {name: 'ember-cli-mdc-elevation'},
-        {name: 'ember-cli-mdc-ripple'},
-        {name: 'ember-cli-mdc-rtl'},
-        {name: 'ember-cli-mdc-typography'},
-        {name: 'ember-cli-mdc-icon'}
-      ]
-    }).then (() => {
-      return this.addPackagesToProject ([
-        {name: '@material/button'},
-      ]);
+    return installPackages (this, [
+      {name: '@material/button'}
+    ]).then (() => {
+      return installAddons ({
+        packages: [
+          {name: 'ember-cli-mdc-elevation'},
+          {name: 'ember-cli-mdc-ripple'},
+          {name: 'ember-cli-mdc-rtl'},
+          {name: 'ember-cli-mdc-typography'},
+          {name: 'ember-cli-mdc-icon'}
+        ]
+      });
     });
   }
 };
