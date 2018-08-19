6e46599d0ae4c717e250c5edcca4da8a0ce7eca6
James H. Hill <hilljh82@gmail.com>
2018-08-19 15:29:38 -0400

    Improved installer experience

--- packages/mdc-card/blueprints/ember-cli-mdc-card/index.js
+++ packages/mdc-card/blueprints/ember-cli-mdc-card/index.js
@@ -1,22 +1,25 @@
 /* eslint-env node */
+
+const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+
 module.exports = {
   description: '',
 
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall () {
-    return this.addAddonsToProject ({
-      packages: [
-        {name: 'ember-cli-mdc-ripple'},
-        {name: 'ember-cli-mdc-elevation'},
-        {name: 'ember-cli-mdc-rtl'},
-        {name: 'ember-cli-mdc-button'},
-        {name: 'ember-cli-mdc-icon-button'}
-      ]
-    }).then (() => {
-      return this.addPackagesToProject ([
-        {name: '@material/card'},
-      ]);
+    return installPackages (this, [
+      {name: '@material/card'},
+    ]).then (() => {
+      return installAddons (this, {
+        packages: [
+          {name: 'ember-cli-mdc-ripple'},
+          {name: 'ember-cli-mdc-elevation'},
+          {name: 'ember-cli-mdc-rtl'},
+          {name: 'ember-cli-mdc-button'},
+          {name: 'ember-cli-mdc-icon-button'}
+        ]
+      })
     });
   }
 
