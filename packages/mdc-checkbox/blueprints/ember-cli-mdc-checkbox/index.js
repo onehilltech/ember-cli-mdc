a5f4e8bb0740195fc10d153ed0a523984b560868
James H. Hill <hilljh82@gmail.com>
2018-08-19 15:41:50 -0400

    Improved installer experience

--- packages/mdc-checkbox/blueprints/ember-cli-mdc-checkbox/index.js
+++ packages/mdc-checkbox/blueprints/ember-cli-mdc-checkbox/index.js
@@ -1,21 +1,26 @@
 /* eslint-env node */
+
+const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+
 module.exports = {
   description: '',
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
+  normalizeEntityName () {
+    // no-op since we're just adding dependencies
+  },
 
   afterInstall () {
-    return this.addAddonsToProject ({
-      packages: [
-        {name: 'ember-cli-mdc-selection-control'},
-        {name: 'ember-cli-mdc-rtl'},
-        {name: 'ember-cli-mdc-form-field'},
-        {name: 'ember-cli-mdc-typography'}
-      ]
-    }).then (() => {
-      return this.addPackagesToProject ([
-        {name: '@material/checkbox'},
-      ]);
+    return installPackages (this, [
+      {name: '@material/checkbox'},
+    ]).then (() => {
+      return installAddons (this, {
+        packages: [
+          {name: 'ember-cli-mdc-selection-control'},
+          {name: 'ember-cli-mdc-rtl'},
+          {name: 'ember-cli-mdc-form-field'},
+          {name: 'ember-cli-mdc-typography'}
+        ]
+      });
     });
   }
 };
--- packages/mdc-dialog/blueprints/ember-cli-mdc-dialog/index.js
+++ packages/mdc-dialog/blueprints/ember-cli-mdc-dialog/index.js
@@ -1,18 +1,23 @@
 /* eslint-env node */
+
+const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+
 module.exports = {
   description: '',
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
+  normalizeEntityName () {
+    // no-op since we're just adding dependencies
+  },
 
   afterInstall () {
-    return this.addAddonsToProject ({
-      packages: [
-        {name: 'ember-cli-mdc-button'}
-      ]
-    }).then (() => {
-      return this.addPackagesToProject ([
-        {name: '@material/dialog'},
-      ]);
+    return installPackages (this, [
+      {name: '@material/dialog'},
+    ]).then (() => {
+      return installAddons (this, {
+        packages: [
+          {name: 'ember-cli-mdc-button'}
+        ]
+      });
     });
   }
 };
