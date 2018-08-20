15ab81ceb67b70014cf9f0c9b635818bc4fc19f3
James H. Hill <hilljh82@gmail.com>
2018-08-19 20:43:21 -0400

    Updated dependencies

--- packages/mdc-form-field/blueprints/ember-cli-mdc-form-field/index.js
+++ packages/mdc-form-field/blueprints/ember-cli-mdc-form-field/index.js
@@ -1,19 +1,24 @@
 /* eslint-env node */
+
+const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+
 module.exports = {
   description: '',
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
+  normalizeEntityName () {
+    // no-op since we're just adding dependencies
+  },
 
   afterInstall () {
-    return this.addAddonsToProject ({
-      packages: [
-        {name: 'ember-cli-mdc-selection-control'},
-        {name: 'ember-cli-mdc-rtl'}
-      ]
-    }).then (() => {
-      return this.addPackagesToProject ([
-        {name: '@material/form-field'},
-      ]);
+    return installPackages (this, [
+      {name: '@material/form-field'},
+    ]).then (() => {
+      return installAddons (this, {
+        packages: [
+          {name: 'ember-cli-mdc-selection-control'},
+          {name: 'ember-cli-mdc-rtl'}
+        ]
+      });
     });
   }
 };
--- packages/mdc-htmlbars/blueprints/ember-cli-mdc-htmlbars/index.js
+++ packages/mdc-htmlbars/blueprints/ember-cli-mdc-htmlbars/index.js
@@ -1,11 +1,16 @@
 /* eslint-env node */
+
+const { installer: { installAddons } } = require ('ember-cli-blueprint-helpers');
+
 module.exports = {
   description: '',
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
+  normalizeEntityName () {
+    // no-op since we're just adding dependencies
+  },
 
   afterInstall () {
-    return this.addAddonsToProject ({
+    return installAddons (this, {
       packages: [
         {name: 'ember-cli-mdc-theme'},
         {name: 'ember-cli-mdc-typography'},
