9a3f56fad3dad8b882aafb8a72a8ef479d43539d
James H. Hill <hilljh82@gmail.com>
2018-08-19 21:16:23 -0400

    Improved install process

--- packages/mdc-textfield/blueprints/ember-cli-mdc-textfield/index.js
+++ packages/mdc-textfield/blueprints/ember-cli-mdc-textfield/index.js
@@ -1,27 +1,30 @@
 /* eslint-env node */
+
+const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+
 module.exports = {
   description: '',
 
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall () {
-    return this.addAddonsToProject ({
-      packages: [
-        {name: 'ember-cli-mdc-animation'},
-        {name: 'ember-cli-mdc-base'},
-        {name: 'ember-cli-mdc-ripple'},
-        {name: 'ember-cli-mdc-rtl'},
-        {name: 'ember-cli-mdc-theme'},
-        {name: 'ember-cli-mdc-typography'},
-        {name: 'ember-cli-mdc-icon'},
-        {name: 'ember-cli-mdc-floating-label'},
-        {name: 'ember-cli-mdc-line-ripple'},
-        {name: 'ember-cli-mdc-notched-outline'}
-      ]
-    }).then (() => {
-      return this.addPackagesToProject ([
-        {name: '@material/textfield'},
-      ]);
+    return installPackages (this, [
+      {name: '@material/textfield'},
+    ]).then (() => {
+      return installAddons (this, {
+        packages: [
+          {name: 'ember-cli-mdc-animation'},
+          {name: 'ember-cli-mdc-base'},
+          {name: 'ember-cli-mdc-ripple'},
+          {name: 'ember-cli-mdc-rtl'},
+          {name: 'ember-cli-mdc-theme'},
+          {name: 'ember-cli-mdc-typography'},
+          {name: 'ember-cli-mdc-icon'},
+          {name: 'ember-cli-mdc-floating-label'},
+          {name: 'ember-cli-mdc-line-ripple'},
+          {name: 'ember-cli-mdc-notched-outline'}
+        ]
+      });
     });
   }
 };
