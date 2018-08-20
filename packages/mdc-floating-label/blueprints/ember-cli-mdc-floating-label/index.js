bd221a452e20dabb12d884c26654323304ebac0d
James H. Hill <hilljh82@gmail.com>
2018-08-19 20:37:56 -0400

    Improved install process

--- packages/mdc-floating-label/blueprints/ember-cli-mdc-floating-label/index.js
+++ packages/mdc-floating-label/blueprints/ember-cli-mdc-floating-label/index.js
@@ -1,22 +1,27 @@
 /* eslint-env node */
+
+const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+
 module.exports = {
   description: '',
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
+  normalizeEntityName () {
+    // no-op since we're just adding dependencies
+  },
 
   afterInstall () {
-    return this.addAddonsToProject ({
-      packages: [
-        {name: 'ember-cli-mdc-rtl'},
-        {name: 'ember-cli-mdc-base'},
-        {name: 'ember-cli-mdc-theme'},
-        {name: 'ember-cli-mdc-typography'},
-        {name: 'ember-cli-mdc-animation'}
-      ]
-    }).then (() => {
-      return this.addPackagesToProject ([
-        {name: '@material/floating-label'},
-      ]);
+    return installPackages (this, [
+      {name: '@material/floating-label'},
+    ]).then (() => {
+      return installAddons (this, {
+        packages: [
+          {name: 'ember-cli-mdc-rtl'},
+          {name: 'ember-cli-mdc-base'},
+          {name: 'ember-cli-mdc-theme'},
+          {name: 'ember-cli-mdc-typography'},
+          {name: 'ember-cli-mdc-animation'}
+        ]
+      });
     });
   }
 };
