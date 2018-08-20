91203040db1d1dd72e7a3d73607186d15a04b6ea
James H. Hill <hilljh82@gmail.com>
2018-08-19 20:58:07 -0400

    Improved install process

--- packages/mdc-ripple/blueprints/ember-cli-mdc-ripple/index.js
+++ packages/mdc-ripple/blueprints/ember-cli-mdc-ripple/index.js
@@ -1,20 +1,23 @@
 /* eslint-env node */
+
+const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+
 module.exports = {
   description: '',
 
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall () {
-    return this.addAddonsToProject ({
-      packages: [
-        {name: 'ember-cli-mdc-animation'},
-        {name: 'ember-cli-mdc-base'},
-        {name: 'ember-cli-mdc-theme'}
-      ]
-    }).then (() => {
-      return this.addPackagesToProject ([
-        {name: '@material/ripple'},
-      ]);
+    return installPackages (this, [
+      {name: '@material/ripple'},
+    ]).then (() => {
+      return installAddons (this, {
+        packages: [
+          {name: 'ember-cli-mdc-animation'},
+          {name: 'ember-cli-mdc-base'},
+          {name: 'ember-cli-mdc-theme'}
+        ]
+      })
     });
   }
 };
