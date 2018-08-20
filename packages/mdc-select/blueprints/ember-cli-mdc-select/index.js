2ce21652b543667410b98f28c27b80bb2787718c
James H. Hill <hilljh82@gmail.com>
2018-08-19 21:00:39 -0400

    Updated dependencies

--- packages/mdc-select/blueprints/ember-cli-mdc-select/index.js
+++ packages/mdc-select/blueprints/ember-cli-mdc-select/index.js
@@ -1,23 +1,26 @@
 /* eslint-env node */
+
+const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+
 module.exports = {
   description: '',
 
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall () {
-    return this.addAddonsToProject ({
-      packages: [
-        {name: 'ember-cli-mdc-floating-label'},
-        {name: 'ember-cli-mdc-line-ripple'},
-        {name: 'ember-cli-mdc-notched-outline'},
-        {name: 'ember-cli-mdc-ripple'},
-        {name: 'ember-cli-mdc-rtl'},
-        {name: 'ember-cli-mdc-typography'}
-      ]
-    }).then (() => {
-      return this.addPackagesToProject ([
-        {name: '@material/select'},
-      ]);
+    return installPackages (this, [
+      {name: '@material/select'},
+    ]).then (() => {
+      return installAddons (this, {
+        packages: [
+          {name: 'ember-cli-mdc-floating-label'},
+          {name: 'ember-cli-mdc-line-ripple'},
+          {name: 'ember-cli-mdc-notched-outline'},
+          {name: 'ember-cli-mdc-ripple'},
+          {name: 'ember-cli-mdc-rtl'},
+          {name: 'ember-cli-mdc-typography'}
+        ]
+      });
     });
   }
 };
