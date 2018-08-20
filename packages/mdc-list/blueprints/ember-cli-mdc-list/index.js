45f90703b53c4d0f03da2415596ca93028f6e0ed
James H. Hill <hilljh82@gmail.com>
2018-08-19 20:55:15 -0400

    Updated dependencies

--- packages/mdc-list/blueprints/ember-cli-mdc-list/index.js
+++ packages/mdc-list/blueprints/ember-cli-mdc-list/index.js
@@ -1,8 +1,13 @@
 /* eslint-env node */
+
+const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+
 module.exports = {
   description: '',
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
+  normalizeEntityName () {
+    // no-op since we're just adding dependencies
+  },
 
   afterInstall () {
     return this.addAddonsToProject ({
