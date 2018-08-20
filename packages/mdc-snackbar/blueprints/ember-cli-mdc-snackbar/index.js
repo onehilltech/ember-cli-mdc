393d799535b7b56cd1ca8b50ce97c60306f93f11
James H. Hill <hilljh82@gmail.com>
2018-08-19 21:12:29 -0400

    Improved install process

--- packages/mdc-snackbar/blueprints/ember-cli-mdc-snackbar/index.js
+++ packages/mdc-snackbar/blueprints/ember-cli-mdc-snackbar/index.js
@@ -1,22 +1,25 @@
 /* eslint-env node */
+
+const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+
 module.exports = {
   description: '',
 
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall () {
-    return this.addAddonsToProject ({
-      packages: [
-        {name: 'ember-cli-mdc-animation'},
-        {name: 'ember-cli-mdc-base'},
-        {name: 'ember-cli-mdc-rtl'},
-        {name: 'ember-cli-mdc-theme'},
-        {name: 'ember-cli-mdc-typography'},
-      ]
-    }).then (() => {
-      return this.addPackagesToProject ([
-        {name: '@material/snackbar'},
-      ]);
+    return installPackages (this, [
+      {name: '@material/snackbar'},
+    ]).then (() => {
+      return installAddons (this, {
+        packages: [
+          {name: 'ember-cli-mdc-animation'},
+          {name: 'ember-cli-mdc-base'},
+          {name: 'ember-cli-mdc-rtl'},
+          {name: 'ember-cli-mdc-theme'},
+          {name: 'ember-cli-mdc-typography'},
+        ]
+      });
     });
   }
 };
