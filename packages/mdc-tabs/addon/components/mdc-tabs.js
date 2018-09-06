4d1cc4257724e7b6dcfab4a0061131f24937b5c1
James H. Hill <hilljh82@gmail.com>
2018-09-05 21:53:39 -0400

    Added support to initial active tab

--- packages/mdc-tabs/addon/components/mdc-tabs.js
+++ packages/mdc-tabs/addon/components/mdc-tabs.js
@@ -28,6 +28,10 @@ export default Component.extend({
     let tabBarElement = this.element.querySelector ('.mdc-tab-bar');
     assert ('The mdc-tabs component must contain a mdc-tab-bar child element.', isPresent (tabBarElement));
 
+    // Initialize the active tab.
+    this._initActiveTab ();
+
+    // Create the tab bar.
     this._tabBar = new mdc.tabBar.MDCTabBar (tabBarElement);
     this._tabBar.listen ('MDCTabBar:activated', this.didActivate.bind (this));
 
@@ -36,9 +40,33 @@ export default Component.extend({
 
     // Verify the number of tabs equals the number of panels.
     assert ('The number of mdc-tab elements must equal the number of mdc-tab-panel elements.', this._tabBar.tabList_.length === this._tabPanels.length);
+  },
 
+  _initActiveTab () {
     let activeTab = this.getWithDefault ('activeTab', 0);
-    this._activateTabPanel (activeTab);
+    let tabs = this.element.querySelectorAll ('.mdc-tab');
+
+    if (activeTab !== 0) {
+      // We need to the active state from the default tab (i.e., tab 0).
+      let tab = tabs[0];
+
+      tab.classList.remove ('mdc-tab--active');
+      tab.querySelector ('.mdc-tab-indicator').classList.remove ('mdc-tab-indicator--active');
+    }
+
+    // Add the active state to the tab.
+    let tab = tabs[activeTab];
+    tab.classList.add ('mdc-tab--active');
+
+    // Add the active state to the tab indicator.
+    let tabIndicator = tab.querySelector ('.mdc-tab-indicator');
+    tabIndicator.classList.add ('mdc-tab-indicator--active');
+
+    // Add the active state to the tab panel.
+    let tabPanel = this.element.querySelectorAll ('.mdc-tab-panel')[activeTab];
+    tabPanel.classList.add ('mdc-tab-panel--active');
+
+    this._currentActiveTab = activeTab;
   },
 
   willDestroyElement () {
