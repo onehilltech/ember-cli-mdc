4d27167f6900581c81d9f2ae6be5ed753c81661c
James H. Hill <hilljh82@gmail.com>
2018-09-07 17:19:58 -0400

    Updated sassOptions

--- packages/mdc-line-ripple/index.js
+++ packages/mdc-line-ripple/index.js
@@ -10,5 +10,19 @@ module.exports = {
       development: 'node_modules/@material/line-ripple/dist/mdc.lineRipple.js',
       production: 'node_modules/@material/line-ripple/dist/mdc.lineRipple.min.js'
     });
+  },
+
+  sassOptions (options) {
+    options.cacheInclude = options.cacheInclude || [];
+    options.cacheInclude.push (/addon.scss/);
+    options.cacheInclude.push (/_app-theme.scss/);
+
+    options.cacheInclude.push (/@material\/animation/);
+    options.cacheInclude.push (/@material\/base/);
+    options.cacheInclude.push (/@material\/line-ripple/);
+    options.cacheInclude.push (/@material\/theme/);
+
+    return options;
+
   }
 };
