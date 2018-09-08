527bed38d75182bcf9bd64b7b41cb0db81e998de
James H. Hill <hilljh82@gmail.com>
2018-09-08 02:14:56 -0400

    Fixed runtime errors; added sass options

--- packages/mdc-htmlbars/addon/-private/text-component.js
+++ packages/mdc-htmlbars/addon/-private/text-component.js
@@ -1,9 +1,9 @@
 import Component from '@ember/component';
 import layout from '../templates/components/text-component';
 
-import { Theme } from 'ember-cli-mdc-theme';
-import { Typography } from 'ember-cli-mdc-typography';
-import { Rtl } from 'ember-cli-mdc-rtl';
+import Theme from 'ember-cli-mdc-theme/mixins/theme';
+import Typography from 'ember-cli-mdc-typography/mixins/typography';
+import Rtl from 'ember-cli-mdc-rtl/mixins/rtl';
 
 export default Component.extend (Theme, Typography, Rtl, {
   layout
--- packages/mdc-htmlbars/index.js
+++ packages/mdc-htmlbars/index.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-mdc-htmlbars'
+  name: 'ember-cli-mdc-htmlbars',
+
+  sassOptions (options) {
+    options.cacheInclude = options.cacheInclude || [];
+    options.cacheInclude.push (/addon\.scss/);
+    options.cacheInclude.push (/_app-theme\.scss/);
+
+    options.cacheInclude.push (/@material\/rtl/);
+    options.cacheInclude.push (/@material\/theme/);
+    options.cacheInclude.push (/@material\/typography/);
+
+    return options;
+  }
 };
