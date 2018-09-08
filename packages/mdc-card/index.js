520f9a71cb1ef011d6b0b413afc5c667dfff079c
James H. Hill <hilljh82@gmail.com>
2018-09-08 02:03:22 -0400

    Updated dependencies; added sass options

--- packages/mdc-card/index.js
+++ packages/mdc-card/index.js
@@ -1,5 +1,24 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-mdc-card'
+  name: 'ember-cli-mdc-card',
+
+  sassOptions (options) {
+    options.cacheInclude = options.cacheInclude || [];
+    options.cacheInclude.push (/addon\.scss/);
+    options.cacheInclude.push (/_app-theme\.scss/);
+
+    options.cacheInclude.push (/@material\/animation/);
+    options.cacheInclude.push (/@material\/base/);
+    options.cacheInclude.push (/@material\/button/);
+    options.cacheInclude.push (/@material\/card/);
+    options.cacheInclude.push (/@material\/elevation/);
+    options.cacheInclude.push (/@material\/icon-button/);
+    options.cacheInclude.push (/@material\/ripple/);
+    options.cacheInclude.push (/@material\/rtl/);
+    options.cacheInclude.push (/@material\/typography/);
+
+    return options;
+  }
 };
+
