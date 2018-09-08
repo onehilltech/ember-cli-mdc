38c3f5978f670241040c1821053e5472777e2640
James H. Hill <hilljh82@gmail.com>
2018-09-07 20:01:44 -0400

    Fixed import error

--- packages/mdc-icon/addon/components/mdc-icon.js
+++ packages/mdc-icon/addon/components/mdc-icon.js
@@ -2,7 +2,8 @@ import Component from '@ember/component';
 import layout from '../templates/components/mdc-icon';
 
 import { computed } from '@ember/object';
-import { TextTheme } from 'ember-cli-mdc-theme';
+
+import TextTheme from 'ember-cli-mdc-theme/mixins/text-theme';
 
 export default Component.extend (TextTheme, {
   layout,
