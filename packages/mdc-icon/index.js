0f7657133e1c7d7631081d9a21c4e80cfabe5bbc
James H. Hill <hilljh82@gmail.com>
2018-09-07 20:01:51 -0400

    Added sass options

--- packages/mdc-icon/index.js
+++ packages/mdc-icon/index.js
@@ -7,6 +7,8 @@ module.exports = {
     this._super (...arguments);
 
     if (process.env.CORBER) {
+      this.ui.writeLine ('Bundling Material Icon fonts with the application.');
+
       // We are using corber to build the application. This means that we need to
       // embed the icons in the application bundle.
       app.import ('node_modules/material-design-icons/iconfont/material-icons.css');
@@ -22,8 +24,17 @@ module.exports = {
 
     if (type === 'head-footer') {
       if (!process.env.CORBER) {
+        this.ui.writeLine ('Linking Material Icon fonts with the application.');
         return '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />';
       }
     }
+  },
+
+  sassOptions (options) {
+    options.cacheInclude = options.cacheInclude || [];
+    options.cacheInclude.push (/addon\.scss/);
+    options.cacheInclude.push (/_app-theme\.scss/);
+
+    return options;
   }
 };
