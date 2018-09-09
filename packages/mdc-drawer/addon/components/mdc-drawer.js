6052fe07f3f111712f119e8401f2ff1ee6fd960f
James H. Hill <hilljh82@gmail.com>
2018-09-09 12:37:17 -0400

    Updated sass integration and to latest version; now possible to switch between modal and dismissible

--- packages/mdc-drawer/addon/components/mdc-drawer.js
+++ packages/mdc-drawer/addon/components/mdc-drawer.js
@@ -1,9 +1,11 @@
+/* global mdc */
+
 import Component from '@ember/component';
 import layout from '../templates/components/mdc-drawer';
 
 import { computed } from '@ember/object';
-import { isPresent } from '@ember/utils';
 import { assert } from '@ember/debug';
+import { isEmpty, isNone } from '@ember/utils';
 
 const STYLES = [
   'dismissible',
@@ -22,32 +24,69 @@ export default Component.extend ({
   styleClassName: computed ('style', function () {
     const style = this.get ('style');
 
+    if (isEmpty (style)) {
+      return null;
+    }
+
     assert (`The style must be one of the following: ${STYLES}`, STYLES.includes (style));
 
     return `mdc-drawer--${style}`;
   }),
 
+  _openEventListener: null,
+
+  _closeEventListener: null,
+
+  _currentStyle: null,
+
+  init () {
+    this._super (...arguments);
+
+    this._openEventListener = this.didOpen.bind (this);
+    this._closeEventListener = this.didClose.bind (this);
+  },
+
   didInsertElement () {
     this._super (...arguments);
 
-    this._drawer = new mdc.drawer.MDCDrawer (this.element);
-    this._drawer.listen ('MDCDrawer:opened', this.didOpen.bind (this));
-    this._drawer.listen ('MDCDrawer:closed', this.didClose.bind (this));
+    this._createComponent ();
 
-    this._drawer.open = this.get ('open');
+    // Set the open state for the component.
+    const { style, open } = this.getProperties (['style', 'open']);
+    this._drawer.open = open;
+
+    // Save the style just in case it changes.
+    this.set ('_currentStyle', style);
+  },
+
+  didUpdateAttrs () {
+    this._super (...arguments);
+
+    // If the style has changed, then we need to delete the original component
+    // before the class for the new style is added to the html. Otherwise, we
+    // we will have a hard time destroying the old component.
+
+    const { style, _currentStyle:currentStyle } = this.getProperties (['style', '_currentStyle']);
+
+    if (style !== currentStyle) {
+      this._destroyComponent ();
+    }
   },
 
   didUpdate () {
     this._super (...arguments);
+
+    if (isNone (this._drawer)) {
+      this._createComponent ();
+      this.set ('_currentStyle', this.get ('style'));
+    }
 
     this._drawer.open = this.get ('open');
   },
 
   willDestroyElement () {
     this._super (...arguments);
-
-    this._drawer.unlisten ('MDCDrawer:opened', this.didOpen.bind (this));
-    this._drawer.unlisten ('MDCDrawer:closed', this.didClose.bind (this));
+    this._destroyComponent ();
   },
 
   didOpen () {
@@ -56,5 +95,22 @@ export default Component.extend ({
 
   didClose () {
     this.set ('open', false);
+  },
+  
+  _createComponent () {
+    if (this._drawer) {
+      this._destroyComponent ();
+    }
+
+    this._drawer = new mdc.drawer.MDCDrawer (this.element);
+    this._drawer.listen ('MDCDrawer:opened', this._openEventListener);
+    this._drawer.listen ('MDCDrawer:closed', this._closeEventListener);
+  },
+
+  _destroyComponent () {
+    this._drawer.unlisten ('MDCDrawer:opened', this._openEventListener);
+    this._drawer.unlisten ('MDCDrawer:closed', this._closeEventListener);
+    this._drawer.destroy ();
+    this._drawer = null;
   }
 });
--- packages/mdc-drawer/tests/dummy/app/controllers/application.js
+++ packages/mdc-drawer/tests/dummy/app/controllers/application.js
@@ -3,6 +3,6 @@ import Controller from '@ember/controller';
 import { equal } from '@ember/object/computed';
 
 export default Controller.extend({
-  style: 'permanent',
-  permanent: equal ('style', 'permanent'),
+  style: 'dismissible',
+  modal: equal ('style', 'modal'),
 });
