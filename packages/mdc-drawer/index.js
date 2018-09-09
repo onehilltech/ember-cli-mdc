f1eb9f703d829362532881b6bfa2e7ab425da757
James H. Hill <hilljh82@gmail.com>
2018-09-09 08:43:56 -0400

    Updated sass integration

--- packages/mdc-drawer/index.js
+++ packages/mdc-drawer/index.js
@@ -10,5 +10,25 @@ module.exports = {
       development: 'node_modules/@material/drawer/dist/mdc.drawer.js',
       production: 'node_modules/@material/drawer/dist/mdc.drawer.min.js'
     });
+  },
+
+  optionsFor (type, options) {
+    if (type === 'sass') {
+      options.cacheInclude = options.cacheInclude || [];
+      options.cacheInclude.push (/addon\.scss/);
+      options.cacheInclude.push (/_app-theme\.scss/);
+
+      options.cacheInclude.push (/@material\/animation/);
+      options.cacheInclude.push (/@material\/base/);
+      options.cacheInclude.push (/@material\/drawer/);
+      options.cacheInclude.push (/@material\/elevation/);
+      options.cacheInclude.push (/@material\/list/);
+      options.cacheInclude.push (/@material\/ripple/);
+      options.cacheInclude.push (/@material\/rtl/);
+      options.cacheInclude.push (/@material\/theme/);
+      options.cacheInclude.push (/@material\/typography/);
+    }
+
+    return options;
   }
 };
