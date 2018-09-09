18b16c3d9cfd7fda81873ecee5375131f150d064
James H. Hill <hilljh82@gmail.com>
2018-09-09 01:30:44 -0400

    Updated sass integratin

--- packages/mdc-form-field/index.js
+++ packages/mdc-form-field/index.js
@@ -10,5 +10,24 @@ module.exports = {
       development: 'node_modules/@material/form-field/dist/mdc.formField.js',
       production: 'node_modules/@material/form-field/dist/mdc.formField.min.js'
     });
+  },
+
+  optionsFor (type, options) {
+    if (type === 'sass') {
+      options.cacheInclude = options.cacheInclude || [];
+      options.cacheInclude.push (/addon\.scss/);
+      options.cacheInclude.push (/_app-theme\.scss/);
+
+      options.cacheInclude.push (/@material\/animation/);
+      options.cacheInclude.push (/@material\/base/);
+      options.cacheInclude.push (/@material\/form-field/);
+      options.cacheInclude.push (/@material\/ripple/);
+      options.cacheInclude.push (/@material\/rtl/);
+      options.cacheInclude.push (/@material\/selection-control/);
+      options.cacheInclude.push (/@material\/theme/);
+      options.cacheInclude.push (/@material\/typography/);
+    }
+
+    return options;
   }
 };
