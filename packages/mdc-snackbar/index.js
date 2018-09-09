737de51cf377b9cc1860dd464c29bba9c940433e
James H. Hill <hilljh82@gmail.com>
2018-09-09 17:35:03 -0400

    Updated sass integration

--- packages/mdc-snackbar/index.js
+++ packages/mdc-snackbar/index.js
@@ -10,5 +10,22 @@ module.exports = {
       development: 'node_modules/@material/snackbar/dist/mdc.snackbar.js',
       production: 'node_modules/@material/snackbar/dist/mdc.snackbar.min.js'
     });
+  },
+
+  optionsFor (type, options) {
+    if (type === 'sass') {
+      options.cacheInclude = options.cacheInclude || [];
+      options.cacheInclude.push (/addon\.scss/);
+      options.cacheInclude.push (/_app-theme\.scss/);
+
+      options.cacheInclude.push (/@material\/animation/);
+      options.cacheInclude.push (/@material\/base/);
+      options.cacheInclude.push (/@material\/rtl/);
+      options.cacheInclude.push (/@material\/snackbar/);
+      options.cacheInclude.push (/@material\/theme/);
+      options.cacheInclude.push (/@material\/typography/);
+    }
+
+    return options;
   }
 };
