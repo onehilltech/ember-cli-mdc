68cbe4c9d4a9ca361d9d1ce3c745d336a99002b2
James H. Hill <hilljh82@gmail.com>
2018-09-09 00:48:52 -0400

    Update sass integration

--- packages/mdc-button/index.js
+++ packages/mdc-button/index.js
@@ -3,15 +3,21 @@
 module.exports = {
   name: 'ember-cli-mdc-button',
 
-  sassOptions (options) {
-    options.cacheInclude = options.cacheInclude || [];
-    options.cacheInclude.push (/addon\.scss/);
-    options.cacheInclude.push (/_app-theme\.scss/);
+  optionsFor (type, options) {
+    if (type === 'sass') {
+      options.cacheInclude = options.cacheInclude || [];
+      options.cacheInclude.push (/addon\.scss/);
+      options.cacheInclude.push (/_app-theme\.scss/);
 
-    options.cacheInclude.push (/@material\/elevation/);
-    options.cacheInclude.push (/@material\/ripple/);
-    options.cacheInclude.push (/@material\/typography/);
-    options.cacheInclude.push (/@material\/button/);
+      options.cacheInclude.push (/@material\/animation/);
+      options.cacheInclude.push (/@material\/base/);
+      options.cacheInclude.push (/@material\/button/);
+      options.cacheInclude.push (/@material\/elevation/);
+      options.cacheInclude.push (/@material\/ripple/);
+      options.cacheInclude.push (/@material\/rtl/);
+      options.cacheInclude.push (/@material\/theme/);
+      options.cacheInclude.push (/@material\/typography/);
+    }
 
     return options;
   }
