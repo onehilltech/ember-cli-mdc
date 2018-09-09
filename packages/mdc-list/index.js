4d8b1872378d390145ae5551f02a2dfdb5ec0540
James H. Hill <hilljh82@gmail.com>
2018-09-09 17:21:22 -0400

    Updated sass integration

--- packages/mdc-list/index.js
+++ packages/mdc-list/index.js
@@ -10,5 +10,23 @@ module.exports = {
       development: 'node_modules/@material/list/dist/mdc.list.js',
       production: 'node_modules/@material/list/dist/mdc.list.min.js'
     });
+  },
+
+  optionsFor (type, options) {
+    if (type === 'sass') {
+      options.cacheInclude = options.cacheInclude || [];
+      options.cacheInclude.push (/addon\.scss/);
+      options.cacheInclude.push (/_app-theme\.scss/);
+
+      options.cacheInclude.push (/@material\/animation/);
+      options.cacheInclude.push (/@material\/base/);
+      options.cacheInclude.push (/@material\/list/);
+      options.cacheInclude.push (/@material\/ripple/);
+      options.cacheInclude.push (/@material\/rtl/);
+      options.cacheInclude.push (/@material\/theme/);
+      options.cacheInclude.push (/@material\/typography/);
+    }
+
+    return options;
   }
 };
