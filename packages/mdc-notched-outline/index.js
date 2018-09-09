e92290be6b6f165dc114c43ff79627ef32e48879
James H. Hill <hilljh82@gmail.com>
2018-09-09 17:27:15 -0400

    Updated sass integration

--- packages/mdc-notched-outline/index.js
+++ packages/mdc-notched-outline/index.js
@@ -10,5 +10,20 @@ module.exports = {
       development: 'node_modules/@material/notched-outline/dist/mdc.notchedOutline.js',
       production: 'node_modules/@material/notched-outline/dist/mdc.notchedOutline.min.js'
     });
+  },
+
+  optionsFor (type, options) {
+    if (type === 'sass') {
+      options.cacheInclude = options.cacheInclude || [];
+      options.cacheInclude.push (/addon\.scss/);
+      options.cacheInclude.push (/_app-theme\.scss/);
+
+      options.cacheInclude.push (/@material\/animation/);
+      options.cacheInclude.push (/@material\/base/);
+      options.cacheInclude.push (/@material\/notched-outline/);
+      options.cacheInclude.push (/@material\/theme/);
+    }
+
+    return options;
   }
 };
