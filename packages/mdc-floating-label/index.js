cadd02660e779ce560b4f623098d7aa5aaf6a37c
James H. Hill <hilljh82@gmail.com>
2018-09-09 16:09:41 -0400

    Updated sass integration

--- packages/mdc-floating-label/index.js
+++ packages/mdc-floating-label/index.js
@@ -10,5 +10,21 @@ module.exports = {
       development: 'node_modules/@material/floating-label/dist/mdc.floatingLabel.js',
       production: 'node_modules/@material/floating-label/dist/mdc.floatingLabel.min.js'
     });
+  },
+
+  optionsFor (type, options) {
+    if (type === 'sass') {
+      options.cacheInclude = options.cacheInclude || [];
+      options.cacheInclude.push (/addon\.scss/);
+      options.cacheInclude.push (/_app-theme\.scss/);
+
+      options.cacheInclude.push (/@material\/animation/);
+      options.cacheInclude.push (/@material\/base/);
+      options.cacheInclude.push (/@material\/floating-label/);
+      options.cacheInclude.push (/@material\/rtl/);
+      options.cacheInclude.push (/@material\/theme/);
+      options.cacheInclude.push (/@material\/typography/);
+    }
+
   }
 };
