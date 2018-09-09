4802fbeab15bb9ea1ac3ffa409fe4e61c6ba8580
James H. Hill <hilljh82@gmail.com>
2018-09-09 18:53:23 -0400

    Updated sass integration

--- packages/mdc-select/index.js
+++ packages/mdc-select/index.js
@@ -10,5 +10,21 @@ module.exports = {
       development: 'node_modules/@material/select/dist/mdc.select.js',
       min: 'node_modules/@material/select/dist/mdc.select.min.js'
     });
+  },
+
+  optionsFor (type, options) {
+    if (type === 'sass') {
+      options.cacheInclude = options.cacheInclude || [];
+      options.cacheInclude.push (/addon\.scss/);
+      options.cacheInclude.push (/_app-theme\.scss/);
+
+      options.cacheInclude.push (/@material\/animation/);
+      options.cacheInclude.push (/@material\/base/);
+      options.cacheInclude.push (/@material\/notched-outline/);
+      options.cacheInclude.push (/@material\/rtl/);
+      options.cacheInclude.push (/@material\/theme/);
+    }
+
+    return options;
   }
 };
