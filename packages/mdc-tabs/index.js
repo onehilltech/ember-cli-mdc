4dae84c226c6dce193458aad33f847476686705c
James H. Hill <hilljh82@gmail.com>
2018-09-10 16:07:53 -0400

    Update sass integration

--- packages/mdc-tabs/index.js
+++ packages/mdc-tabs/index.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-mdc-tabs'
+  name: 'ember-cli-mdc-tabs',
+
+  optionsFor (type, options) {
+    if (type === 'sass') {
+      options.cacheInclude = options.cacheInclude || [];
+      options.cacheInclude.push (/addon\.scss/);
+      options.cacheInclude.push (/_app-theme\.scss/);
+
+      options.cacheInclude.push (/@material\/animation/);
+      options.cacheInclude.push (/@material\/base/);
+      options.cacheInclude.push (/@material\/elevation/);
+      options.cacheInclude.push (/@material\/ripple/);
+      options.cacheInclude.push (/@material\/rtl/);
+      options.cacheInclude.push (/@material\/tab/);
+      options.cacheInclude.push (/@material\/tab-bar/);
+      options.cacheInclude.push (/@material\/tab-indicator/);
+      options.cacheInclude.push (/@material\/tab-scroller/);
+      options.cacheInclude.push (/@material\/theme/);
+      options.cacheInclude.push (/@material\/typography/);
+    }
+
+    return options;
+  }
 };
