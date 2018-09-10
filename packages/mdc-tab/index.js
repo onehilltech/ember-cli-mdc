79f90e61762cf86421e188c4009a200929c832fc
James H. Hill <hilljh82@gmail.com>
2018-09-10 00:04:03 -0400

    Updated sass integration

--- packages/mdc-tab/index.js
+++ packages/mdc-tab/index.js
@@ -10,5 +10,22 @@ module.exports = {
       development: 'node_modules/@material/tab/dist/mdc.tab.js',
       production: 'node_modules/@material/tab/dist/mdc.tab.min.js'
     });
+  },
+
+  sassOptions (options) {
+    options.cacheInclude = options.cacheInclude || [];
+    options.cacheInclude.push (/addon\.scss/);
+    options.cacheInclude.push (/_app-theme\.scss/);
+
+    options.cacheInclude.push (/@material\/animation/);
+    options.cacheInclude.push (/@material\/base/);
+    options.cacheInclude.push (/@material\/ripple/);
+    options.cacheInclude.push (/@material\/rtl/);
+    options.cacheInclude.push (/@material\/tab/);
+    options.cacheInclude.push (/@material\/tab-indicator/);
+    options.cacheInclude.push (/@material\/theme/);
+    options.cacheInclude.push (/@material\/typography/);
+
+    return options;
   }
 };
