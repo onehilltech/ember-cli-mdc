4e32a692615f697a6c395cc58a77caf004a93acf
James H. Hill <hilljh82@gmail.com>
2018-09-09 21:56:56 -0400

    Updated sass integration

--- packages/mdc-switch/index.js
+++ packages/mdc-switch/index.js
@@ -10,5 +10,24 @@ module.exports = {
       development: 'node_modules/@material/switch/dist/mdc.switch.js',
       production: 'node_modules/@material/switch/dist/mdc.switch.min.js'
     });
+  },
+
+  optionsFor (type, options) {
+    if (type === 'sass') {
+      options.cacheInclude = options.cacheInclude || [];
+      options.cacheInclude.push (/addon\.scss/);
+      options.cacheInclude.push (/_app-theme\.scss/);
+
+      options.cacheInclude.push (/@material\/animation/);
+      options.cacheInclude.push (/@material\/base/);
+      options.cacheInclude.push (/@material\/elevation/);
+      options.cacheInclude.push (/@material\/ripple/);
+      options.cacheInclude.push (/@material\/rtl/);
+      options.cacheInclude.push (/@material\/selection-control/);
+      options.cacheInclude.push (/@material\/switch/);
+      options.cacheInclude.push (/@material\/theme/);
+    }
+
+    return options;
   }
 };
