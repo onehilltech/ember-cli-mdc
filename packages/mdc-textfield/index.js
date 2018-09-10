9cf7a07545bda4ccaa2d50916aa1f62c6f7b59f9
James H. Hill <hilljh82@gmail.com>
2018-09-09 23:31:54 -0400

    Updated sass integration

--- packages/mdc-textfield/index.js
+++ packages/mdc-textfield/index.js
@@ -10,5 +10,24 @@ module.exports = {
       development: 'node_modules/@material/textfield/dist/mdc.textfield.js',
       production: 'node_modules/@material/textfield/dist/mdc.textfield.min.js'
     });
+  },
+
+  sassOptions (options) {
+    options.cacheInclude = options.cacheInclude || [];
+    options.cacheInclude.push (/addon\.scss/);
+    options.cacheInclude.push (/_app-theme\.scss/);
+
+    options.cacheInclude.push (/@material\/animation/);
+    options.cacheInclude.push (/@material\/base/);
+    options.cacheInclude.push (/@material\/floating-label/);
+    options.cacheInclude.push (/@material\/line-ripple/);
+    options.cacheInclude.push (/@material\/notched-outline/);
+    options.cacheInclude.push (/@material\/ripple/);
+    options.cacheInclude.push (/@material\/rtl/);
+    options.cacheInclude.push (/@material\/textfield/);
+    options.cacheInclude.push (/@material\/theme/);
+    options.cacheInclude.push (/@material\/typography/);
+
+    return options;
   }
 };
