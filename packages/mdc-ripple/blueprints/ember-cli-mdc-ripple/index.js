66d8eeae295bd349e9431170c914436a42a29045
James H. Hill <hilljh82@gmail.com>
2018-09-13 11:55:19 -0400

    Updated blueprint to improve performance

--- packages/mdc-ripple/blueprints/ember-cli-mdc-ripple/index.js
+++ packages/mdc-ripple/blueprints/ember-cli-mdc-ripple/index.js
@@ -1,27 +1,15 @@
 /* eslint-env node */
 
-const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
-module.exports = {
-  description: '',
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/ripple'}
+  ],
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
-
-  afterInstall () {
-    return installPackages (this, [
-      {name: '@material/ripple'},
-    ]).then (() => {
-      return installAddons (this, {
-        packages: [
-          {name: 'ember-cli-mdc-animation'},
-          {name: 'ember-cli-mdc-base'},
-          {name: 'ember-cli-mdc-theme'}
-        ]
-      })
-    });
-  },
-
-  sassOptions (options) {
-
-  }
-};
+  addons: [
+    {name: 'ember-cli-mdc-animation'},
+    {name: 'ember-cli-mdc-base'},
+    {name: 'ember-cli-mdc-theme'}
+  ]
+});
