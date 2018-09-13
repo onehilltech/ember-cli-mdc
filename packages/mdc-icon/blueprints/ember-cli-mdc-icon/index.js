447a7630fb4b9569ce9737a80fe4fdf79c1dbcb4
James H. Hill <hilljh82@gmail.com>
2018-09-13 11:33:46 -0400

    Updated blueprint to improve performance

--- packages/mdc-icon/blueprints/ember-cli-mdc-icon/index.js
+++ packages/mdc-icon/blueprints/ember-cli-mdc-icon/index.js
@@ -1,21 +1,14 @@
 /* eslint-env node */
 
-const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
-module.exports = {
-  description: '',
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: 'material-design-icons'}
+  ],
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
+  addons: [
+    {name: 'ember-cli-mdc-theme'}
+  ]
+});
 
-  afterInstall () {
-    return installPackages (this, [
-      {name: 'material-design-icons'}
-    ]).then (() => {
-      return installAddons (this, {
-        packages: [
-          {name: 'ember-cli-mdc-theme' }
-        ]
-      });
-    });
-  }
-};
