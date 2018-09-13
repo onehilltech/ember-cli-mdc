4797b24426cec38f5447022722c97156961a9392
James H. Hill <hilljh82@gmail.com>
2018-09-13 12:03:29 -0400

    Updated blueprint to improve performance

--- packages/mdc-select/blueprints/ember-cli-mdc-select/index.js
+++ packages/mdc-select/blueprints/ember-cli-mdc-select/index.js
@@ -1,26 +1,18 @@
 /* eslint-env node */
 
-const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
-module.exports = {
-  description: '',
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/select'}
+  ],
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
-
-  afterInstall () {
-    return installPackages (this, [
-      {name: '@material/select'},
-    ]).then (() => {
-      return installAddons (this, {
-        packages: [
-          {name: 'ember-cli-mdc-floating-label'},
-          {name: 'ember-cli-mdc-line-ripple'},
-          {name: 'ember-cli-mdc-notched-outline'},
-          {name: 'ember-cli-mdc-ripple'},
-          {name: 'ember-cli-mdc-rtl'},
-          {name: 'ember-cli-mdc-typography'}
-        ]
-      });
-    });
-  }
-};
+  addons: [
+    {name: 'ember-cli-mdc-floating-label'},
+    {name: 'ember-cli-mdc-line-ripple'},
+    {name: 'ember-cli-mdc-notched-outline'},
+    {name: 'ember-cli-mdc-ripple'},
+    {name: 'ember-cli-mdc-rtl'},
+    {name: 'ember-cli-mdc-typography'}
+  ]
+});
