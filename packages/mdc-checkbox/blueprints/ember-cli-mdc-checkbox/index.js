bf7853ce160773a143e152cba9bd7656c7059958
James H. Hill <hilljh82@gmail.com>
2018-09-13 02:19:24 -0400

    Updated blueprint to improve performance

--- packages/mdc-checkbox/blueprints/ember-cli-mdc-checkbox/index.js
+++ packages/mdc-checkbox/blueprints/ember-cli-mdc-checkbox/index.js
@@ -1,26 +1,16 @@
 /* eslint-env node */
 
-const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
-module.exports = {
-  description: '',
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/checkbox'}
+  ],
 
-  normalizeEntityName () {
-    // no-op since we're just adding dependencies
-  },
-
-  afterInstall () {
-    return installPackages (this, [
-      {name: '@material/checkbox'},
-    ]).then (() => {
-      return installAddons (this, {
-        packages: [
-          {name: 'ember-cli-mdc-selection-control'},
-          {name: 'ember-cli-mdc-rtl'},
-          {name: 'ember-cli-mdc-form-field'},
-          {name: 'ember-cli-mdc-typography'}
-        ]
-      });
-    });
-  }
-};
+  addons: [
+    {name: 'ember-cli-mdc-selection-control'},
+    {name: 'ember-cli-mdc-rtl'},
+    {name: 'ember-cli-mdc-form-field'},
+    {name: 'ember-cli-mdc-typography'},
+  ]
+});
