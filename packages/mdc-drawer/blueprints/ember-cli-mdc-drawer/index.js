d10ccba6369b844137a25f60eb918ff146e77b2d
James H. Hill <hilljh82@gmail.com>
2018-09-13 02:23:24 -0400

    Updated blueprint to improve performance

--- packages/mdc-drawer/blueprints/ember-cli-mdc-drawer/index.js
+++ packages/mdc-drawer/blueprints/ember-cli-mdc-drawer/index.js
@@ -1,25 +1,15 @@
 /* eslint-env node */
 
-const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
-module.exports = {
-  description: '',
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/drawer'}
+  ],
 
-  normalizeEntityName () {
-    // no-op since we're just adding dependencies
-  },
-
-  afterInstall () {
-    return installPackages (this, [
-      {name: '@material/drawer'},
-    ]).then (() => {
-      return installAddons (this, {
-        packages: [
-          {name: 'ember-cli-mdc-list'},
-          {name: 'ember-cli-mdc-base'},
-          {name: 'ember-cli-mdc-elevation'}
-        ]
-      });
-    });
-  }
-};
+  addons: [
+    {name: 'ember-cli-mdc-list'},
+    {name: 'ember-cli-mdc-base'},
+    {name: 'ember-cli-mdc-elevation'}
+  ]
+});
