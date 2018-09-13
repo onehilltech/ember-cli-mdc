a48b6156d5cfff00a9da0c6336f8e8156994e972
James H. Hill <hilljh82@gmail.com>
2018-09-13 02:13:56 -0400

    Updated blueprint to improve performance

--- packages/mdc-rtl/blueprints/ember-cli-mdc-rtl/index.js
+++ packages/mdc-rtl/blueprints/ember-cli-mdc-rtl/index.js
@@ -1,17 +1,9 @@
 /* eslint-env node */
 
-const { installer: { installPackages } } = require ('ember-cli-blueprint-helpers');
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
-module.exports = {
-  description: '',
-
-  normalizeEntityName () {
-    // no-op since we're just adding dependencies
-  },
-
-  afterInstall () {
-    return installPackages (this, [
-      {name: '@material/rtl'}
-    ]);
-  }
-};
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/rtl'}
+  ]
+});
