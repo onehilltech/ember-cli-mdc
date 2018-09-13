5cee1c9b804beba21591c80a65de9372eca1c4a9
James H. Hill <hilljh82@gmail.com>
2018-09-13 13:00:38 -0400

    Updated blueprint to improve performance

--- packages/mdc-tab-bar/blueprints/ember-cli-mdc-tab-bar/index.js
+++ packages/mdc-tab-bar/blueprints/ember-cli-mdc-tab-bar/index.js
@@ -1,22 +1,14 @@
 /* eslint-env node */
 
-const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
-module.exports = {
-  description: '',
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/tab-bar'}
+  ],
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
-
-  afterInstall () {
-    return installPackages (this, [
-      {name: '@material/tab-bar'},
-    ]).then (() => {
-      return installAddons (this, {
-        packages: [
-          {name: 'ember-cli-mdc-elevation'},
-          {name: 'ember-cli-mdc-tab-scroller'}
-        ]
-      });
-    });
-  }
-};
+  addons: [
+    {name: 'ember-cli-mdc-elevation'},
+    {name: 'ember-cli-mdc-tab-scroller'}
+  ]
+});
