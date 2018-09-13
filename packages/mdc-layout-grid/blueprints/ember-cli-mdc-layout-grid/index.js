c37558e843f09893c20c709c00848c74de5f34cb
James H. Hill <hilljh82@gmail.com>
2018-09-13 11:38:59 -0400

    Updated blueprint to improve performance

--- packages/mdc-layout-grid/blueprints/ember-cli-mdc-layout-grid/index.js
+++ packages/mdc-layout-grid/blueprints/ember-cli-mdc-layout-grid/index.js
@@ -1,15 +1,9 @@
 /* eslint-env node */
 
-const { installer: { installPackages } } = require ('ember-cli-blueprint-helpers');
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
-module.exports = {
-  description: '',
-
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
-
-  afterInstall () {
-    return installPackages (this, [
-      {name: '@material/layout-grid'},
-    ]);
-  }
-};
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/layout-grid'}
+  ]
+});
