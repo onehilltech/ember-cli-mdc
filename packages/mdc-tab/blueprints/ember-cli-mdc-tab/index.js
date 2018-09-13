66831b60c7791aeb858159c60960a5c6675c2cea
James H. Hill <hilljh82@gmail.com>
2018-09-13 12:56:54 -0400

    Updated blueprint to improve performance

--- packages/mdc-tab/blueprints/ember-cli-mdc-tab/index.js
+++ packages/mdc-tab/blueprints/ember-cli-mdc-tab/index.js
@@ -1,24 +1,16 @@
 /* eslint-env node */
 
-const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
-module.exports = {
-  description: '',
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/tab'}
+  ],
 
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
-
-  afterInstall () {
-    return installPackages (this, [
-      {name: '@material/tab'},
-    ]).then (() => {
-      return installAddons (this, {
-        packages: [
-          {name: 'ember-cli-mdc-ripple'},
-          {name: 'ember-cli-mdc-typography'},
-          {name: 'ember-cli-mdc-rtl'},
-          {name: 'ember-cli-mdc-tab-indicator'},
-        ]
-      });
-    });
-  }
-};
+  addons: [
+    {name: 'ember-cli-mdc-ripple'},
+    {name: 'ember-cli-mdc-typography'},
+    {name: 'ember-cli-mdc-rtl'},
+    {name: 'ember-cli-mdc-tab-indicator'}
+  ]
+});
