ac1ccfaf358bb7f9aaee7165ab5341913e89a8f7
James H. Hill <hilljh82@gmail.com>
2018-09-13 11:48:10 -0400

    Updated blueprint to improve performance

--- packages/mdc-notched-outline/blueprints/ember-cli-mdc-notched-outline/index.js
+++ packages/mdc-notched-outline/blueprints/ember-cli-mdc-notched-outline/index.js
@@ -1,25 +1,16 @@
 /* eslint-env node */
 
-const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
-module.exports = {
-  description: '',
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/notched-outline'}
+  ],
 
-  normalizeEntityName () {
-    // no-op since we're just adding dependencies
-  },
+  addons: [
+    {name: 'ember-cli-mdc-animation'},
+    {name: 'ember-cli-mdc-base'},
+    {name: 'ember-cli-mdc-theme'}
+  ]
+});
 
-  afterInstall () {
-    return installPackages (this, [
-      {name: '@material/notched-outline'},
-    ]).then (() => {
-      return installAddons (this, {
-        packages: [
-          {name: 'ember-cli-mdc-animation'},
-          {name: 'ember-cli-mdc-base'},
-          {name: 'ember-cli-mdc-theme'}
-        ]
-      });
-    });
-  }
-};
