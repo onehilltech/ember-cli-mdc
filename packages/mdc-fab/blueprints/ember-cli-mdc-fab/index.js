3d81346764dd074dc0e6c5ad5ed18aee27febee4
James H. Hill <hilljh82@gmail.com>
2018-09-13 08:55:49 -0400

    Updated blueprint to improve performance

--- packages/mdc-fab/blueprints/ember-cli-mdc-fab/index.js
+++ packages/mdc-fab/blueprints/ember-cli-mdc-fab/index.js
@@ -1,27 +1,17 @@
 /* eslint-env node */
 
-const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
-module.exports = {
-  description: '',
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/fab'}
+  ],
 
-  normalizeEntityName () {
-    // no-op since we're just adding dependencies
-  },
-
-  afterInstall () {
-    return installPackages (this, [
-      {name: '@material/fab'},
-    ]).then (() => {
-      return installAddons (this, {
-        packages: [
-          {name: 'ember-cli-mdc-elevation'},
-          {name: 'ember-cli-mdc-ripple'},
-          {name: 'ember-cli-mdc-rtl'},
-          {name: 'ember-cli-mdc-typography'},
-          {name: 'ember-cli-mdc-icon'}
-        ]
-      });
-    });
-  }
-};
+  addons: [
+    {name: 'ember-cli-mdc-elevation'},
+    {name: 'ember-cli-mdc-ripple'},
+    {name: 'ember-cli-mdc-rtl'},
+    {name: 'ember-cli-mdc-typography'},
+    {name: 'ember-cli-mdc-icon'}
+  ]
+});
