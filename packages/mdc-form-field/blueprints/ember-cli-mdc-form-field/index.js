ca756ab2c19bab263dcff0351edf604b722af82d
James H. Hill <hilljh82@gmail.com>
2018-09-13 09:17:57 -0400

    Updated blueprint to improve performance

--- packages/mdc-form-field/blueprints/ember-cli-mdc-form-field/index.js
+++ packages/mdc-form-field/blueprints/ember-cli-mdc-form-field/index.js
@@ -1,24 +1,14 @@
 /* eslint-env node */
 
-const { installer: { installAddons, installPackages } } = require ('ember-cli-blueprint-helpers');
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
-module.exports = {
-  description: '',
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/form-field'}
+  ],
 
-  normalizeEntityName () {
-    // no-op since we're just adding dependencies
-  },
-
-  afterInstall () {
-    return installPackages (this, [
-      {name: '@material/form-field'},
-    ]).then (() => {
-      return installAddons (this, {
-        packages: [
-          {name: 'ember-cli-mdc-selection-control'},
-          {name: 'ember-cli-mdc-rtl'}
-        ]
-      });
-    });
-  }
-};
+  addons: [
+    {name: 'ember-cli-mdc-selection-control'},
+    {name: 'ember-cli-mdc-rtl'}
+  ]
+});
