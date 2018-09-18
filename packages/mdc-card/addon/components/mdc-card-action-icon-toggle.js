ee4b3d7f2bc00d51200bef8d57c7b891dbe5a119
James H. Hill <hilljh82@gmail.com>
2018-09-18 12:24:11 -0400

    Fixed import statement

--- packages/mdc-card/addon/components/mdc-card-action-icon-toggle.js
+++ packages/mdc-card/addon/components/mdc-card-action-icon-toggle.js
@@ -1,4 +1,4 @@
-import IconButtonToggleComponent from 'mdc-icon-button/components/mdc-icon-button-toggle';
+import IconButtonToggleComponent from 'ember-cli-mdc-icon-button/components/mdc-icon-button-toggle';
 import CardAction from '../mixins/card-action';
 
 export default IconButtonToggleComponent.extend (CardAction, {
