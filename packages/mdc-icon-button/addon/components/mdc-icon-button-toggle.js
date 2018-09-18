41cd12d7c6471c279718b78f2c86a356e51f90e6
James H. Hill <hilljh82@gmail.com>
2018-09-18 12:01:38 -0400

    Toggle button did not update 'on' attribute

--- packages/mdc-icon-button/addon/components/mdc-icon-button-toggle.js
+++ packages/mdc-icon-button/addon/components/mdc-icon-button-toggle.js
@@ -4,7 +4,6 @@ import Component from '@ember/component';
 import layout from '../templates/components/mdc-icon-button-toggle';
 
 import { computed } from '@ember/object';
-import { isPresent } from '@ember/utils';
 
 function noOp () {}
 
@@ -50,19 +49,24 @@ export default Component.extend({
   didInsertElement () {
     this._super (...arguments);
 
-    this._iconToggleButton = new mdc.iconButton.MDCIconButtonToggle (this.element);
-    this._iconToggleButton.listen ('MDCIconButtonToggle:change', this._changeEventListener);
-
+    // Set the attributes on the element.
     this.element.setAttribute ('aria-hidden', true);
     this.element.setAttribute ('aria-pressed', false);
+
+    this._iconToggleButton = new mdc.iconButton.MDCIconButtonToggle (this.element);
+
+    // Initialize the on button, then set the listener. We do not want the listener
+    // being called just for initializing the button.
+    this._iconToggleButton.on = this.getWithDefault ('on', false);
+    this._iconToggleButton.listen ('MDCIconButtonToggle:change', this._changeEventListener);
   },
 
-  didRender () {
+  didUpdate () {
     this._super (...arguments);
 
-    const on = this.get ('on');
+    const on = this.getWithDefault ('on', false);
 
-    if (isPresent (on)) {
+    if (on !== this._iconToggleButton.on) {
       this._iconToggleButton.on = on;
     }
   },
@@ -73,8 +77,11 @@ export default Component.extend({
     this._iconToggleButton.unlisten ('MDCIconButtonToggle:change', this._changeEventListener);
   },
 
-  didChange (ev) {
-    const { detail: {isOn}} = ev;
+  didChange ({detail: {isOn}}) {
+    // Update the on state to reflect the changes, then notify the action that
+    // there was a change in state.
+
+    this.set ('on', isOn);
     this.getWithDefault ('toggle', noOp) (isOn);
   }
 }).reopenClass ({
