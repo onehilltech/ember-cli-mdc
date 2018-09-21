f0aacb92f2e27cd80d7cc18d62155a111a2663d9
James H. Hill <hilljh82@gmail.com>
2018-09-21 16:58:36 -0400

    Set replace true as default

--- packages/mdc-top-app-bar/addon/components/mdc-top-app-bar-navigate-up-to.js
+++ packages/mdc-top-app-bar/addon/components/mdc-top-app-bar-navigate-up-to.js
@@ -1,13 +1,16 @@
 import LinkComponent from '@ember/routing/link-component';
 
 export default LinkComponent.extend ({
-  classNames: ['mdc-top-app-bar__navigation-icon', 'material-icons'],
-
   init () {
     this._super (...arguments);
 
     // This component should never have a block. We can just add the
     // back arrow as the first parameter.
     this.get ('params').unshift ('arrow_back');
-  }
+  },
+
+  classNames: ['mdc-top-app-bar__navigation-icon', 'material-icons'],
+
+  /// By default, we replace the history.
+  replace: true
 });
