e7607910d51b043aab2ae3e7034275d2686eead4
James H. Hill <hilljh82@gmail.com>
2018-09-26 14:36:08 -0400

    Selecting a chip changes the selected value

--- packages/mdc-chips/tests/dummy/app/routes/index.js
+++ packages/mdc-chips/tests/dummy/app/routes/index.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
+import { A } from '@ember/array';
 
-export default Route.extend({
+export default Route.extend ({
+  setupController (controller) {
+    this._super (...arguments);
+
+    controller.setProperties ({
+      options: A ([
+        {id: 'extra_small', text: 'Extra Small'},
+        {id: 'small', text: 'Small'},
+        {id: 'medium', text: 'Medium'},
+        {id: 'large', text: 'Large'},
+        {id: 'extra_large', text: 'Extra Large'}
+      ]),
+
+      choice: 'medium'
+    });
+
+
+  }
 });
