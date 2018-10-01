9856488a6567db8937398f366492e288bf8efba4
James H. Hill <hilljh82@gmail.com>
2018-10-01 17:06:13 -0400

    Added ripple enhancement to button

--- packages/mdc-button/addon/components/mdc-button-linkto.js
+++ packages/mdc-button/addon/components/mdc-button-linkto.js
@@ -1,6 +1,22 @@
+/* global mdc */
+
 import LinkComponent from '@ember/routing/link-component';
 import ButtonMixin from '../mixins/button';
 
-export default LinkComponent.extend (ButtonMixin, {
+const { MDCRipple } = mdc.ripple;
 
+export default LinkComponent.extend (ButtonMixin, {
+  _buttonRipple: null,
+
+  didInsertElement () {
+    this._super (...arguments);
+
+    this._buttonRipple = new MDCRipple (this.element);
+  },
+
+  willDestroyElement () {
+    this._super (...arguments);
+
+    this._buttonRipple.destroy ();
+  }
 });
