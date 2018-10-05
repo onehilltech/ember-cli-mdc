da72e93fe17bbc0e9e0ebaf85bc3e8afb165c532
James H. Hill <james@onehilltech.com>
2018-10-05 16:10:57 -0400

    Allow you to set primary and secondary color programmtically

--- packages/mdc-theme/addon/mixins/theme.js
+++ packages/mdc-theme/addon/mixins/theme.js
@@ -4,6 +4,44 @@ import SurfaceTheme from './surface-theme';
 import BackgroundTheme from './background-theme';
 import TextTheme from './text-theme';
 
-export default Mixin.create (TextTheme, BackgroundTheme, SurfaceTheme, {
+import { isPresent } from '@ember/utils';
 
+export default Mixin.create (TextTheme, BackgroundTheme, SurfaceTheme, {
+  /// @{ CSS Custom Properties
+
+  /// Set the --mdc-theme-primary property.
+  themePrimaryColor: null,
+
+  /// Set the --mdc-theme-secondary property.
+  themeSecondaryColor: null,
+
+  /// @}
+
+  didInsertElement () {
+    this._super (...arguments);
+
+    let { themePrimaryColor, themeSecondaryColor } = this.getProperties (['themePrimaryColor', 'themeSecondaryColor']);
+
+    if (isPresent (themePrimaryColor))
+      this.element.style.setProperty ('--mdc-theme-primary', themePrimaryColor);
+
+    if (isPresent (themeSecondaryColor))
+      this.element.style.setProperty ('--mdc-theme-secondary', themeSecondaryColor);
+  },
+
+  didUpdateAttrs () {
+    this._super (...arguments);
+
+    let { themePrimaryColor, themeSecondaryColor } = this.getProperties (['themePrimaryColor', 'themeSecondaryColor']);
+
+    if (isPresent (themePrimaryColor))
+      this.element.style.setProperty ('--mdc-theme-primary', themePrimaryColor);
+    else
+      this.element.style.removeProperty ('--mdc-theme-primary');
+
+    if (isPresent (themeSecondaryColor))
+      this.element.style.setProperty ('--mdc-theme-secondary', themeSecondaryColor);
+    else
+      this.element.style.removeProperty ('--mdc-theme-secondary');
+  }
 });
