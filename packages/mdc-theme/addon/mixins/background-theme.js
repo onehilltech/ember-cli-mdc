921c2b9a7665842caa1fb1e6174b7dc129122b0c
James H. Hill <james@onehilltech.com>
2018-10-05 15:34:00 -0400

    themeBackgroundColor now updates the --mdc-theme-background CSS custom property

--- packages/mdc-theme/addon/mixins/background-theme.js
+++ packages/mdc-theme/addon/mixins/background-theme.js
@@ -1,6 +1,6 @@
 import Mixin from '@ember/object/mixin';
 import { computed } from '@ember/object';
-import { isEmpty } from '@ember/utils';
+import { isEmpty, isPresent } from '@ember/utils';
 
 import { assert } from '@ember/debug';
 
@@ -19,11 +19,20 @@ export default Mixin.create ({
    * Sets the background color to the selected style. The `themeBackground` attribute must
    * be one of the following values:
    *
-   *  = background
-   *  = primary
-   *  = secondary
+   *  = "background"
+   *  = "primary"
+   *  = "secondary"
    */
   themeBackground: null,
+
+  /// {@ CSS Custom Properties
+
+  /**
+   * Defines the CSS custom property --mdc-theme-background.
+   */
+  themeBackgroundColor: null,
+
+  /// @}
 
   mdcThemeBackgroundClassName: computed ('themeBackground', function () {
     const themeBackground = this.get ('themeBackground');
@@ -34,5 +43,31 @@ export default Mixin.create ({
 
     assert (`The themeBackground attribute must be one of the following values: ${THEME_BACKGROUND_VALUES}`, THEME_BACKGROUND_VALUES.includes (themeBackground));
     return  themeBackground === 'background' ? 'mdc-theme--background' : `mdc-theme--${themeBackground}-bg`;
-  })
+  }),
+
+  didInsertElement () {
+    this._super (...arguments);
+
+    let themeBackgroundColor = this.get ('themeBackgroundColor');
+
+    if (isPresent (themeBackgroundColor)) {
+      this.element.style.setProperty ('--mdc-theme-background', themeBackgroundColor);
+    }
+    else {
+      this.element.style.removeProperty ('--mdc-theme-background');
+    }
+  },
+
+  didUpdateAttrs () {
+    this._super (...arguments);
+
+    let themeBackgroundColor = this.get ('themeBackgroundColor');
+
+    if (isPresent (themeBackgroundColor)) {
+      this.element.style.setProperty ('--mdc-theme-background', themeBackgroundColor);
+    }
+    else {
+      this.element.style.removeProperty ('--mdc-theme-background');
+    }
+  }
 });
