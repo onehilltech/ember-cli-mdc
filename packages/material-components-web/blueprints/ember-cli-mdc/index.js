a5a8a879d68d43d9405bb14f490207f11d9df764
James H. Hill <james@onehilltech.com>
2018-10-10 20:16:38 -0400

    updated ember-cli-mdc blueprint

--- packages/material-components-web/blueprints/ember-cli-mdc/index.js
+++ packages/material-components-web/blueprints/ember-cli-mdc/index.js
@@ -9,13 +9,22 @@ module.exports = {
       packages: [
         {name: 'ember-cli-mdc-button'},
         {name: 'ember-cli-mdc-card'},
+        {name: 'ember-cli-mdc-checkbox'},
+        {name: 'ember-cli-mdc-chips'},
         {name: 'ember-cli-mdc-dialog'},
         {name: 'ember-cli-mdc-drawer'},
         {name: 'ember-cli-mdc-fab'},
         {name: 'ember-cli-mdc-form'},
+        {name: 'ember-cli-mdc-htmlbars'},
         {name: 'ember-cli-mdc-layout-grid'},
+        {name: 'ember-cli-mdc-linear-progress'},
         {name: 'ember-cli-mdc-list'},
+        {name: 'ember-cli-mdc-select'},
         {name: 'ember-cli-mdc-snackbar'},
+        {name: 'ember-cli-mdc-stepper'},
+        {name: 'ember-cli-mdc-switch'},
+        {name: 'ember-cli-mdc-tabs'},
+        {name: 'ember-cli-mdc-textfield'},
         {name: 'ember-cli-mdc-top-app-bar'}
       ]
     });
