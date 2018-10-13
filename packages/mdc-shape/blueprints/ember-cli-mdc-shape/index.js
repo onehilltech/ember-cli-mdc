f17adcb559ea3775f876957d9abebb2e2087672e
James H. Hill <james@onehilltech.com>
2018-10-13 01:54:14 -0400

    Added placeholder package

---
+++ packages/mdc-shape/blueprints/ember-cli-mdc-shape/index.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
+
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/shape'}
+  ],
+
+  addons: [
+    {name: 'ember-cli-mdc-sass', blueprintOptions: {save: true}}
+  ]
+});
