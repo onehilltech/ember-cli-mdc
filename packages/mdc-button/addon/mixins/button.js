e47bac90bb3de5e8361e5b71be925f6786334327
James H. Hill <james@onehilltech.com>
2018-10-14 15:43:38 -0400

    Added theme mixin to button

--- packages/mdc-button/addon/mixins/button.js
+++ packages/mdc-button/addon/mixins/button.js
@@ -1,10 +1,11 @@
 import Mixin from '@ember/object/mixin';
 import Ripple from 'ember-cli-mdc-ripple/mixins/ripple';
+import Theme from 'ember-cli-mdc-theme/mixins/theme';
 
 import { computed } from '@ember/object';
 import { isPresent } from '@ember/utils';
 
-export default Mixin.create (Ripple, {
+export default Mixin.create (Theme, Ripple, {
   classNames: ['mdc-button'],
 
   classNameBindings: ['mdcStyleClassName', 'dense:mdc-button--dense'],
