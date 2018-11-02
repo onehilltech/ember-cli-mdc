19925422bc76e3219e55830757250d50c8085a97
James H. Hill <james@onehilltech.com>
2018-11-02 14:19:18 -0400

    Removed jquery to support FastBoot

--- packages/mdc-notched-outline/addon/components/mdc-notched-outline.js
+++ packages/mdc-notched-outline/addon/components/mdc-notched-outline.js
@@ -3,7 +3,9 @@
 import Component from '@ember/component';
 import layout from '../templates/components/mdc-notched-outline';
 
-import $ from 'jquery';
+import { isPresent } from '@ember/utils';
+
+const { MDCNotchedOutline } = mdc.notchedOutline;
 
 export default Component.extend({
   layout,
@@ -13,22 +15,33 @@ export default Component.extend({
 
   _notchedOutline: null,
 
-  $notchedOutlineIdle: null,
+  /// The idle element automatically added as a sibling to this component.
+  _notchedOutlineIdle: null,
 
   didInsertElement () {
     this._super (...arguments);
 
-    this._notchedOutline = new mdc.notchedOutline.MDCNotchedOutline (this.element);
-
-    // Let's add the idle element after this element.
-    this.$notchedOutlineIdle = $('<div class="mdc-notched-outline__idle"></div>');
-    this.$notchedOutlineIdle.insertAfter (this.$ ());
+    this._insertNotchedOutlineIdle ();
+    this._notchedOutline = new MDCNotchedOutline (this.element);
   },
 
   willDestroyElement () {
     this._super (...arguments);
 
-    this.$notchedOutlineIdle.remove ();
+    // Destroy the component, and then remove the notched outline idle element
+    // we added when the component element was inserted into the DOM.
+
     this._notchedOutline.destroy ();
+
+    if (isPresent (this._notchedOutlineIdle)) {
+      this._notchedOutlineIdle.remove ();
+    }
+  },
+
+  _insertNotchedOutlineIdle () {
+    this._notchedOutlineIdle = document.createElement ('div');
+    this._notchedOutlineIdle.classList.add ('mdc-notched-outline__idle');
+
+    this.element.parentElement.insertBefore (this._notchedOutlineIdle, this.element.nextSibling);
   }
 });
