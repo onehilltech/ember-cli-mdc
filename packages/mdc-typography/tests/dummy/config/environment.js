de638054fed99ab52d2902382591406313b0e1f6
James H. Hill <james@onehilltech.com>
2018-11-04 12:54:35 -0500

    Updated the test config

--- packages/mdc-typography/tests/dummy/config/environment.js
+++ packages/mdc-typography/tests/dummy/config/environment.js
@@ -20,13 +20,7 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-      rootElement: '.mdc'
-    },
 
-    'ember-cli-mdc': {
-      typography: {
-        disabled: false
-      }
     }
   };
 
