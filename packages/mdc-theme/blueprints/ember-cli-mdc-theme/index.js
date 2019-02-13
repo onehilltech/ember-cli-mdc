80c9c8d172365733572fb04d48fe3d043e5f5ad4
James H. Hill <james@onehilltech.com>
2019-02-13 10:22:37 -0500

    fix: Fixed version targeting

--- packages/mdc-theme/blueprints/ember-cli-mdc-theme/index.js
+++ packages/mdc-theme/blueprints/ember-cli-mdc-theme/index.js
@@ -4,7 +4,7 @@ const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
 module.exports = Blueprint.extend ({
   packages: [
-    {name: '@material/theme', target: '0.43.1'}
+    {name: '@material/theme', target: '0.43.0'}
   ],
 
   addons: [
