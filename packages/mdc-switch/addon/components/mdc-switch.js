e5f4186ef8a1227ce0a6a26d68df67339b56e4d9
James H. Hill <james@onehilltech.com>
2019-02-12 20:26:16 -0500

    fix: The checked state was incorrect when control was updated between page changes

--- packages/mdc-switch/addon/components/mdc-switch.js
+++ packages/mdc-switch/addon/components/mdc-switch.js
@@ -3,6 +3,8 @@
 import Component from '@ember/component';
 import layout from '../templates/components/mdc-switch';
 
+const CLASSNAME_CHECKED = 'mdc-switch--checked';
+
 export default Component.extend({
   layout,
 
@@ -12,6 +14,20 @@ export default Component.extend({
   disabled: false,
 
   _switch: null,
+
+  didUpdate () {
+    this._super (...arguments);
+
+    // Make sure the state of the element matches the state of this component.
+    const checked = this.checked;
+
+    if (checked !== this.element.classList.contains (CLASSNAME_CHECKED)) {
+      if (checked)
+        this.element.classList.add (CLASSNAME_CHECKED);
+      else
+        this.element.classList.remove (CLASSNAME_CHECKED);
+    }
+  },
 
   didInsertElement () {
     this._super (...arguments);
