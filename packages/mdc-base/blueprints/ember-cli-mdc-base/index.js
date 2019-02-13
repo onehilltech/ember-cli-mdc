1012d860fdde6c7182b5134d105006504e28353b
James H. Hill <james@onehilltech.com>
2019-02-13 10:13:38 -0500

    fix: Fixed version targeting

--- packages/mdc-base/blueprints/ember-cli-mdc-base/index.js
+++ packages/mdc-base/blueprints/ember-cli-mdc-base/index.js
@@ -4,6 +4,6 @@ const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
 module.exports = Blueprint.extend ({
   packages: [
-    {name: '@material/base', target: '0.43.1'}
+    {name: '@material/base', target: '0.41.0'}
   ]
 });
