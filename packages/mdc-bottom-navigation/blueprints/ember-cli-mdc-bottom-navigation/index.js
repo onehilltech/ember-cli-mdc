478d30202adf0596390d30bb73e4d3907c721652
James H. Hill <james@onehilltech.com>
2019-04-25 15:58:24 -0400

    fix: Removed unused dependencies

--- packages/mdc-bottom-navigation/blueprints/ember-cli-mdc-bottom-navigation/index.js
+++ packages/mdc-bottom-navigation/blueprints/ember-cli-mdc-bottom-navigation/index.js
@@ -7,5 +7,6 @@ module.exports = Blueprint.extend ({
     {name: 'ember-cli-mdc-theme'},
     {name: 'ember-cli-mdc-icon'},
     {name: 'ember-cli-mdc-ripple'},
+    {name: 'ember-cli-mdc-typography'}
   ]
 });
