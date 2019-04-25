479c38d637b98ff990ad5ca291e003d1e19a3f55
James H. Hill <james@onehilltech.com>
2019-04-25 14:53:53 -0400

    chore: Moved logic from ripple component to ripple mixin

--- packages/mdc-ripple/addon/mixins/ripple.js
+++ packages/mdc-ripple/addon/mixins/ripple.js
@@ -15,4 +15,27 @@ export default Mixin.create ({
 
   // Set the ripple color. The acceptable values are "primary" or "accent".
   rippleColor: null,
+
+  /// The instance of the MDCRipple component.
+  _ripple: null,
+
+  /// Force the creation of the MDC ripple component. This is only required if the
+  /// target MDC component does not create a ripple component.
+  createRippleComponent: false,
+
+  didInsertElement () {
+    this._super (...arguments);
+
+    if (this.createRippleComponent) {
+      this._ripple = new mdc.ripple.MDCRipple (this.element);
+    }
+  },
+
+  willDestroyElement () {
+    this._super (...arguments);
+
+    if (!!this._ripple) {
+      this._ripple.destroy ();
+    }
+  }
 });
--- packages/mdc-ripple/app/components/mdc-ripple.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-cli-mdc-ripple/components/mdc-ripple';
