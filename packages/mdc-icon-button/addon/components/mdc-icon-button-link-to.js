6c614a580ef9db98775078ed83fb8818abd46c45
James H. Hill <james@onehilltech.com>
2019-04-25 16:34:09 -0400

    fix: The components did not use the ripple mixin

--- packages/mdc-icon-button/addon/components/mdc-icon-button-link-to.js
+++ packages/mdc-icon-button/addon/components/mdc-icon-button-link-to.js
@@ -1,12 +1,16 @@
 import LinkComponent from '@ember/routing/link-component';
+import RippleMixin from 'ember-cli-mdc-ripple/mixins/ripple';
+
 import layout from '../templates/components/mdc-icon-button-link-to';
 
 import { computed } from '@ember/object';
 
-export default LinkComponent.extend({
+export default LinkComponent.extend (RippleMixin, {
   layout,
 
   classNames: ['mdc-icon-button', 'material-icons'],
+
+  createRippleComponent: true,
 
   icon: computed ('params.[]', function () {
     return this.get ('params')[0];
--- packages/mdc-icon-button/addon/components/mdc-icon-button.js
+++ packages/mdc-icon-button/addon/components/mdc-icon-button.js
@@ -1,15 +1,19 @@
-import RippleComponent from 'ember-cli-mdc-ripple/components/mdc-ripple';
+import Component from '@ember/component';
+import RippleMixin from 'ember-cli-mdc-ripple/mixins/ripple';
+
 import layout from '../templates/components/mdc-icon-button';
 
 import { computed } from '@ember/object';
 
-export default RippleComponent.extend ({
+export default Component.extend (RippleMixin, {
   layout,
 
   tagName: 'button',
 
   classNames: ['mdc-icon-button', 'material-icons'],
   attributeBindings: ['disabled', 'value', 'type', 'form'],
+
+  createRippleComponent: true,
 
   icon: computed ('params.[]', function () {
     return this.get ('params')[0];
