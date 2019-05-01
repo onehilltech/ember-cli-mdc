e705f4337b41a838b01400ecd2dd8d4a1448eb38
James H. Hill <james@onehilltech.com>
2019-05-01 01:36:51 -0400

    chore: upgrade animation package

--- packages/mdc-animation/blueprints/ember-cli-mdc-animation/index.js
+++ packages/mdc-animation/blueprints/ember-cli-mdc-animation/index.js
@@ -4,7 +4,7 @@ const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
 module.exports = Blueprint.extend ({
   packages: [
-    {name: '@material/animation', target: '0.41.0'}
+    {name: '@material/animation', target: '1.0.0'}
   ],
 
   addons: [
