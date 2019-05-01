e6118b003fcb824e148126d7b89fe2b72a20ec6e
James H. Hill <james@onehilltech.com>
2019-05-01 10:35:49 -0400

    chore: upgraded ripple package

--- packages/mdc-ripple/tests/dummy/app/components/mdc-foo.js
+++ packages/mdc-ripple/tests/dummy/app/components/mdc-foo.js
@@ -1,10 +1,12 @@
 import Component from '@ember/component';
 import layout from '../templates/components/mdc-foo';
 
-import { Ripple } from 'ember-cli-mdc-ripple';
+import Ripple from 'ember-cli-mdc-ripple/mixins/ripple';
 
 export default Component.extend (Ripple, {
   layout,
+
+  tagName: 'span',
 
   createRippleComponent: true
 });
