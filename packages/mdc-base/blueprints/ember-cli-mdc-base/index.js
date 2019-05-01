1d31ea5c5c5683b71b6d94366be3bc649df6b900
James H. Hill <james@onehilltech.com>
2019-05-01 01:45:49 -0400

    chore: upgraded base package

--- packages/mdc-base/blueprints/ember-cli-mdc-base/index.js
+++ packages/mdc-base/blueprints/ember-cli-mdc-base/index.js
@@ -4,6 +4,6 @@ const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
 module.exports = Blueprint.extend ({
   packages: [
-    {name: '@material/base', target: '0.41.0'}
+    {name: '@material/base', target: '1.0.0'}
   ]
 });
