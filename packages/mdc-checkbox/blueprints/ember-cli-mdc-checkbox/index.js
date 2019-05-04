5c6ad6a554f7f4e0179e8a968937dd310ddac004
James H. Hill <james@onehilltech.com>
2019-05-04 10:17:26 -0400

    chore: upgraded package

--- packages/mdc-checkbox/blueprints/ember-cli-mdc-checkbox/index.js
+++ packages/mdc-checkbox/blueprints/ember-cli-mdc-checkbox/index.js
@@ -4,7 +4,7 @@ const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
 module.exports = Blueprint.extend ({
   packages: [
-    {name: '@material/checkbox', target: '0.43.0'}
+    {name: '@material/checkbox', target: '2.0.0'}
   ],
 
   addons: [
