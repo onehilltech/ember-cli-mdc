8dc9be71f7efb83ce0ed881882a565e1a0dd5bac
James H. Hill <james@onehilltech.com>
2019-05-06 19:52:29 -0400

    feat: added support for full ripple effects

--- packages/mdc-list/addon/mixins/list-item.js
+++ packages/mdc-list/addon/mixins/list-item.js
@@ -1,15 +1,22 @@
 import Mixin from '@ember/object/mixin';
+import RippleMixin from 'ember-cli-mdc-ripple/mixins/ripple';
 
-export default Mixin.create ({
+export default Mixin.create (RippleMixin, {
   classNames: ['mdc-list-item'],
 
-  classNameBindings: ['selected:mdc-list-item--selected', 'activated:mdc-list-item--activated', 'disabled:mdc-list-item--disabled'],
+  classNameBindings: [
+    'selected:mdc-list-item--selected',
+    'activated:mdc-list-item--activated',
+    'disabled:mdc-list-item--disabled'
+  ],
   
   attributeBindings: ['role'],
 
   selected: false,
 
   activated: false,
+
+  createRippleComponent: true,
 
   didInsertElement () {
     this._super (...arguments);
