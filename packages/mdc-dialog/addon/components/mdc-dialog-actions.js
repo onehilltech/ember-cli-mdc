77c8af979e1bdaabeaff9acd5767afe846575ab8
James H. Hill <james@onehilltech.com>
2019-05-06 15:17:09 -0400

    feat: Added a mdc-dialog-actions component

---
+++ packages/mdc-dialog/addon/components/mdc-dialog-actions.js
@@ -0,0 +1,10 @@
+import Component from '@ember/component';
+import layout from '../templates/components/mdc-dialog-actions';
+
+export default Component.extend({
+  layout,
+
+  classNames: ['mdc-dialog__actions'],
+
+  tagName: 'footer'
+});
