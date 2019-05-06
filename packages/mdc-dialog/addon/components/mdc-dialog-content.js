2158e5a53d2a187cad43f4d30a06d13d37b254c3
James H. Hill <james@onehilltech.com>
2019-05-06 14:52:08 -0400

    feat: Added ember components for all mdc dialog components

---
+++ packages/mdc-dialog/addon/components/mdc-dialog-content.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import layout from '../templates/components/mdc-dialog-content';
+
+export default Component.extend({
+  layout,
+
+  classNames: ['mdc-dialog__content']
+});
