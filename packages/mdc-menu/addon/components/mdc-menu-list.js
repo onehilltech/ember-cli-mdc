5f810392973554e34300bdf2f193f39b7916dc26
James H. Hill <james@onehilltech.com>
2019-05-06 22:39:54 -0400

    fix: Added missing aria attributes

--- packages/mdc-menu/addon/components/mdc-menu-list.js
+++ packages/mdc-menu/addon/components/mdc-menu-list.js
@@ -1,5 +1,14 @@
 import List from 'ember-cli-mdc-list/components/mdc-list';
 
 export default List.extend({
-  role: 'menu'
+  role: 'menu',
+
+  attributeBindings: [
+    'orientation:aria-orientation',
+    'tabindex'
+  ],
+
+  orientation: 'vertical',
+
+  tabindex: -1
 });
