7bac8c52426a728ee3598e9757798298a006eda3
James H. Hill <james@onehilltech.com>
2019-05-07 21:12:03 -0400

    chore: added snackbar surface component

---
+++ packages/mdc-snackbar/addon/components/mdc-snackbar-surface.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import layout from '../templates/components/mdc-snackbar-surface';
+
+export default Component.extend({
+  layout,
+
+  classNames: ['mdc-snackbar__surface']
+});
