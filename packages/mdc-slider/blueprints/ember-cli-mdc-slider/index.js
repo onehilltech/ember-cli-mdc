40e0acb2198a0852646ab0926eb8729aa97a2f0d
James H. Hill <james@onehilltech.com>
2019-05-07 20:54:18 -0400

    chore: upgraded component package

--- packages/mdc-slider/blueprints/ember-cli-mdc-slider/index.js
+++ packages/mdc-slider/blueprints/ember-cli-mdc-slider/index.js
@@ -4,7 +4,7 @@ const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
 module.exports = Blueprint.extend ({
   packages: [
-    {name: '@material/slider', target: '0.43.0'}
+    {name: '@material/slider', target: '1.1.0'}
   ],
 
   addons: [
