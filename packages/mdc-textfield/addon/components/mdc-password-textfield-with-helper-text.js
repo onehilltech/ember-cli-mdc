75dc5d7d02e1da3c09951b06719aad9f8df3e709
James Hill <james@onehilltech.com>
2019-10-03 23:51:16 -0500

    feat: added password requirements support to password text field

--- packages/mdc-textfield/addon/components/mdc-password-textfield-with-helper-text.js
+++ packages/mdc-textfield/addon/components/mdc-password-textfield-with-helper-text.js
@@ -3,6 +3,10 @@ import layout from '../templates/components/mdc-password-textfield-with-helper-t
 
 import HelperTextSupport from '../mixins/helper-text-support';
 
+import { computed } from '@ember/object';
+import { isEmpty } from '@ember/utils';
+import { empty, not, readOnly, or } from '@ember/object/computed';
+
 export default Component.extend (HelperTextSupport, {
   layout,
 
@@ -13,6 +17,27 @@ export default Component.extend (HelperTextSupport, {
   iconOn: 'visibility',
 
   iconOff: 'visibility_off',
+
+  // A collection of requirements for the password.
+  passwordRequirements: null,
+
+  // Get the first requirement that fails the password.
+  failedPasswordRequirements: computed ('value', 'passwordRequirements.[]', function () {
+    const { value, passwordRequirements: requirements } = this.getProperties (['value','passwordRequirements']);
+    return isEmpty (value) || isEmpty (requirements) ? null : A (requirements.filter (req => value.match (req.pattern) === null));
+  }),
+
+  meetsPasswordRequirements: empty ('failedPasswordRequirements'),
+
+  invalidPassword: not ('meetsPasswordRequirements'),
+
+  firstFailedPasswordRequirement: readOnly ('failedPasswordRequirements.firstObject'),
+
+  passwordRequirementsErrorMessage: readOnly ('firstFailedPasswordRequirement.description'),
+
+  passwordErrorMessage: null,
+
+  errorMessage: or ('passwordErrorMessage', 'passwordRequirementsErrorMessage'),
 
   actions: {
     toggle () {
