1980c1d1170790af662765af93c9b7e0ca49a4ac
James Hill <james@onehilltech.com>
2019-11-19 22:54:00 -0600

    fix: data table had the wrong tag name

--- packages/mdc-data-table/addon/components/mdc-data-table-table.js
+++ packages/mdc-data-table/addon/components/mdc-data-table-table.js
@@ -2,6 +2,8 @@ import Component from '@ember/component';
 import layout from '../templates/components/mdc-data-table-table';
 
 export default Component.extend({
+  tagName: 'table',
+
   layout,
 
   classNames: ['mdc-data-table__table'],
