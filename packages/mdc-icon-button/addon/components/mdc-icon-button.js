3c62138b6be111cca53273d06c853a9b3593a80a
James Hill <james@onehilltech.com>
2020-01-18 02:15:21 -0500

    feat: added icon button mixin

--- packages/mdc-icon-button/addon/components/mdc-icon-button.js
+++ packages/mdc-icon-button/addon/components/mdc-icon-button.js
@@ -1,26 +1,14 @@
 import Component from '@ember/component';
-import RippleMixin from 'ember-cli-mdc-ripple/mixins/ripple';
+import IconButtonMixin from '../mixins/icon-button';
 
 import layout from '../templates/components/mdc-icon-button';
 
 import { computed } from '@ember/object';
 
-export default Component.extend (RippleMixin, {
+export default Component.extend (IconButtonMixin, {
   layout,
 
   tagName: 'button',
-
-  classNames: ['mdc-icon-button', 'material-icons'],
-
-  attributeBindings: [
-    'disabled',
-    'value',
-    'type',
-    'form'
-  ],
-
-  createRippleComponent: true,
-  unbounded: true,
 
   icon: computed ('params.[]', function () {
     return this.get ('params')[0];
