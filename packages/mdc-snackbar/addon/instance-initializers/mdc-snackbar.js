4853fdaa715f38169de9397569468460e0ddace7
James Hill <james@onehilltech.com>
2020-03-05 19:30:18 -0500

    fix: show warning message if ember app does not have an application route

--- packages/mdc-snackbar/addon/instance-initializers/mdc-snackbar.js
+++ packages/mdc-snackbar/addon/instance-initializers/mdc-snackbar.js
@@ -2,7 +2,13 @@ import Snackbar from 'ember-cli-mdc-snackbar/mixins/route/snackbar';
 
 export function initialize (appInstance) {
   let appRoute = appInstance.lookup ('route:application');
-  appRoute.reopen (Snackbar);
+
+  if (isPresent (appRoute)) {
+    appRoute.reopen (Snackbar);
+  }
+  else {
+    console.warn ('The ember application does not have an application route. The ember-cli-snackbar add-on cannot integrate the application-level snackbar.');
+  }
 }
 
 export default {
