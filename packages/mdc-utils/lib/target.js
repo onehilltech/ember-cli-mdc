4a5d404b13e7072493008f3eb600209c0651bb37
James Hill <james@onehilltech.com>
2020-06-01 07:32:16 -0400

    fix: target version for prerelease was incorrect

--- packages/mdc-utils/lib/target.js
+++ packages/mdc-utils/lib/target.js
@@ -9,7 +9,7 @@ function target (version) {
   if (prerelease.length !== 0) {
     // Let's update to the latest pre-release version.
     let coerced = semver.coerce (version);
-    return `^${coerced}-${prerelease[0]}-0`;
+    return `^${coerced}-${prerelease[0]}.0`;
   }
   else {
     // Let's update the latest minor version.
