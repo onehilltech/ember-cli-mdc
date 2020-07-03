b9d1a86fbd3dcb20ba483b635c7541407a0d32a7
James Hill <james@onehilltech.com>
2020-07-02 21:28:58 -0400

    fix: switched to get/set from object module

--- packages/mdc-snackbar/addon/mixins/route/snackbar.js
+++ packages/mdc-snackbar/addon/mixins/route/snackbar.js
@@ -1,11 +1,13 @@
 import Mixin from '@ember/object/mixin';
+import { set, get } from '@ember/object';
 
 export default Mixin.create({
   snackbarPropertyName: 'snackbar',
 
   actions: {
     'app:snackbar' (opts) {
-      this.controller.set (this.get ('snackbarPropertyName'), opts);
+      const snackbarPropertyName = get (this, 'snackbarPropertyName');
+      set (this.controller, snackbarPropertyName, opts);
     }
   }
 });
