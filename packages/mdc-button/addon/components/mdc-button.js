a2a10f777542964d7c343f5dc94f1058dc7b1286
James Hill <james@onehilltech.com>
2020-11-22 00:43:40 -0500

    fix: allow type to override by subclasses

--- packages/mdc-button/addon/components/mdc-button.js
+++ packages/mdc-button/addon/components/mdc-button.js
@@ -6,6 +6,10 @@ const { MDCRipple } = mdc.ripple;
 export default class MdcButtonComponent extends Component {
   _button = null;
 
+  get type () {
+    return this.args.type;
+  }
+
   @action
   didInsert (element) {
     this._button = new MDCRipple (element);
