4b0a4e40dd1d1864914f3f9df1d4676c8d26ba98
James Hill <james@onehilltech.com>
2020-11-22 02:00:45 -0500

    fix: added type information to card icon buttons

--- packages/mdc-card/addon/components/mdc-card-action-icon.js
+++ packages/mdc-card/addon/components/mdc-card-action-icon.js
@@ -1,5 +1,7 @@
 import MdcIconButtonComponent from 'ember-cli-mdc-icon-button/components/mdc-icon-button';
 
 export default class MdcCardActionIconComponent extends MdcIconButtonComponent {
-
+  get type () {
+    return 'mdc-card__action mdc-card__action--icon';
+  }
 }
