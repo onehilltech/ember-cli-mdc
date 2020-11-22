01a633131237627b3b342bc2641fefc96f2299b1
James Hill <james@onehilltech.com>
2020-11-22 02:03:59 -0500

    fix: added type property to icon button

--- packages/mdc-card/addon/components/mdc-card-action-icon-toggle.js
+++ packages/mdc-card/addon/components/mdc-card-action-icon-toggle.js
@@ -1,5 +1,7 @@
 import IconButtonToggleComponent from 'ember-cli-mdc-icon-button/components/mdc-icon-button-toggle';
 
-export default IconButtonToggleComponent.extend (CardAction, {
-  classNames: ['mdc-card__action--icon']
-});
+export default class MdcCardActionIconToggleComponent extends IconButtonToggleComponent {
+  get type () {
+    return 'mdc-card__action mdc-card__action--icon';
+  }
+}
