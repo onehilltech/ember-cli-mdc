68650902bcc100ca4fbc82a45de42ecdfb50e757
James Hill <james@onehilltech.com>
2020-11-23 16:07:19 -0500

    chore: adding uncommitted files

---
+++ packages/mdc-linear-progress/tests/dummy/app/controllers/index.js
@@ -0,0 +1,33 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from "@glimmer/tracking";
+
+export default class IndexController extends Controller {
+  @tracked
+  started = false;
+
+  @tracked
+  progress = 0;
+
+  @tracked
+  count = 0;
+
+  @action
+  toggleProgress () {
+    this.started = !this.started;
+    this._doHeartbeat ();
+  }
+
+  _doHeartbeat () {
+    if (this.started) {
+      setTimeout (function (component) {
+        component._updateProgress ();
+        component._doHeartbeat ();
+      }, 1000, this);
+    }
+  }
+
+  _updateProgress () {
+    this.progress = this.count ++ / 100;
+  }
+}
