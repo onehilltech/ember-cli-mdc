025f572d43d779cfbc02895925530041ba23779a
James Hill <james@onehilltech.com>
2020-11-23 22:09:20 -0500

    chore: updated to ember 3.18 and material 6.0.0

--- packages/mdc-switch/addon/components/mdc-switch-native-control.js
+++ packages/mdc-switch/addon/components/mdc-switch-native-control.js
@@ -1,11 +1,5 @@
 import Checkbox from '@ember/component/checkbox';
 
-export default class MdcSwitchNativeControlComponent extends Checkbox {
-  classNames = ['mdc-switch__native-control'];
-
-  didInsertElement () {
-    super.didInsertElement (...arguments);
-
-    this.element.setAttribute ('role', 'switch');
-  }
-}
+export default Checkbox.extend ({
+  classNames: ['mdc-switch__native-control']
+})
