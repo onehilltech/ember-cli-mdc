1d26a17893c4fb40e3bc5dd109931d07d8436b6a
James Hill <james@onehilltech.com>
2020-11-24 23:29:22 -0500

    chore: ran codemods for 3.18

--- packages/mdc-radio/tests/integration/components/mdc-radio-with-label-test.js
+++ packages/mdc-radio/tests/integration/components/mdc-radio-with-label-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | mdc-radio-with-label', function(hooks) {
 
     await render(hbs`<MdcRadioWithLabel />`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | mdc-radio-with-label', function(hooks) {
       </MdcRadioWithLabel>
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
