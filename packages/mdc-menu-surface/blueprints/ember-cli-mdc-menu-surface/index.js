f053ee9fb7036becbca58d8691338cff5612355b
James Hill <james@onehilltech.com>
2020-11-24 20:22:51 -0500

    feat: added menu surface modifier

--- packages/mdc-menu-surface/blueprints/ember-cli-mdc-menu-surface/index.js
+++ packages/mdc-menu-surface/blueprints/ember-cli-mdc-menu-surface/index.js
@@ -14,6 +14,7 @@ module.exports = Blueprint.extend ({
     {name: 'ember-cli-mdc-rtl', target},
     {name: 'ember-cli-mdc-shape', target},
     {name: 'ember-cli-mdc-base', target},
+    {name: 'ember-modifier'},
     {name: '@ember/render-modifiers'}
   ]
 });
