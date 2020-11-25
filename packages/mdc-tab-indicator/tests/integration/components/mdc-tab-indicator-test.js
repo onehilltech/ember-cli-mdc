211e007533f1a752e7b490d91cb967e63a2c64dc
James Hill <james@onehilltech.com>
2020-11-25 14:01:48 -0500

    chore: ran codemods

--- packages/mdc-tab-indicator/tests/integration/components/mdc-tab-indicator-test.js
+++ packages/mdc-tab-indicator/tests/integration/components/mdc-tab-indicator-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | mdc-tab-indicator', function(hooks) {
 
     await render(hbs`{{mdc-tab-indicator}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | mdc-tab-indicator', function(hooks) {
       {{/mdc-tab-indicator}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
