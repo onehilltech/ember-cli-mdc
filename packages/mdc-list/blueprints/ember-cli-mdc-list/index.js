72534932d8b8b51647ef4a89895a6875bb39d844
James Hill <james@onehilltech.com>
2020-11-24 20:34:20 -0500

    chore: updated config

--- packages/mdc-list/blueprints/ember-cli-mdc-list/index.js
+++ packages/mdc-list/blueprints/ember-cli-mdc-list/index.js
@@ -6,7 +6,7 @@ const target = require ('ember-cli-mdc-utils').target (version);
 
 module.exports = Blueprint.extend ({
   packages: [
-    {name: '@material/list', target: '3.2.0'}
+    {name: '@material/list', target: '^6.0.0'}
   ],
 
   addons: [
