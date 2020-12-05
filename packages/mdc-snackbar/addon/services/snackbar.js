36fe75303577d4dcefa9cbb4715cf3fc6e20c260
James Hill <james@onehilltech.com>
2020-12-05 10:29:47 -0500

    chore: adding uncommitted files

---
+++ packages/mdc-snackbar/addon/services/snackbar.js
@@ -0,0 +1,163 @@
+import Service from '@ember/service';
+
+import { isPresent } from '@ember/utils';
+import { guidFor } from '@ember/object/internals';
+import { action } from '@ember/object';
+import { assert } from '@ember/debug';
+
+function noOp () { }
+
+const { MDCSnackbar } = mdc.snackbar;
+
+export default class SnackbarService extends Service {
+  _snackbar = null;
+
+  _openingListener = null;
+  _openedListener = null;
+  _closingListener = null;
+  _closedListener = null;
+
+  constructor () {
+    super (...arguments);
+
+    this._openingListener = this.willOpen.bind (this);
+    this._openedListener = this.didOpen.bind (this);
+    this._closingListener = this.willClose.bind (this);
+    this._closedListener = this.didClose.bind (this);
+  }
+
+  destroy () {
+    super.destroy ();
+
+    this._cleanup ();
+  }
+
+  /**
+   * Show the snackbar to the user.
+   *
+   * @param options
+   */
+  @action
+  show (options) {
+    // First, clean up everything.
+    this._cleanup ();
+
+    // Build the snackbar.
+    this._snackbar = this._build (options);
+    this._snackbar.open ();
+  }
+
+  /**
+   * Build a new snackbar component for the options.
+   *
+   * @param options
+   * @private
+   */
+  _build (options) {
+    const {
+      timeout = 4000,
+      closeOnEscape = true,
+      message,
+      action,
+      dismiss,
+      opening = noOp,
+      opened = noOp,
+      closing = noOp,
+      closed = noOp,
+    } = options;
+
+    assert ('The timeout must be between the value of 4000 and 10000', timeout >= 4000 && timeout <= 10000);
+
+    let html = `
+      <div class="mdc-snackbar">
+        <div class="mdc-snackbar__surface">
+          <div class="mdc-snackbar__label" role="status" aria-live="polite">${message}</div>
+          ${isPresent (action) || isPresent (dismiss) ? this._actionButtons (action, dismiss) : ''}
+        </div>
+      </div>`;
+
+    // Create the snackbar html elements, and append it to the document.
+    let snackbarFragment = document.createRange ().createContextualFragment (html);
+    let snackbarElement = snackbarFragment.querySelector ('.mdc-snackbar');
+    snackbarElement.id = guidFor (snackbarElement);
+    document.body.appendChild (snackbarElement);
+
+    // Create and initialize the snackbar component.
+    let snackbar = new MDCSnackbar (snackbarElement);
+    snackbar.timeoutMs = timeout;
+    snackbar.closeOnEscape = closeOnEscape;
+
+    snackbar.listen ('MDCSnackbar:opening', () => opening ());
+    snackbar.listen ('MDCSnackbar:opened', () => opened ());
+
+    snackbar.listen ('MDCSnackbar:closing', (ev) => closing (ev.detail));
+    snackbar.listen ('MDCSnackbar:closed', (ev) => {
+      const {reason} = ev.detail;
+
+      switch (reason) {
+        case 'action':
+          (action.click || noOp) ();
+          break;
+
+        case 'dismiss':
+          (dismiss && dismiss.click || noOp) ();
+          break;
+      }
+
+      // Notify the general listener of the event.
+      closed (ev.detail);
+    });
+
+    return snackbar;
+  }
+
+  _actionButtons (action, dismiss) {
+    return `<div class="mdc-snackbar__actions">
+              ${isPresent (action) ? this._actionButton (action.label) : ''}
+              ${isPresent (dismiss) ? this._dismissButton (dismiss.icon) : ''}
+            </div>`;
+  }
+
+  _actionButton (label) {
+    return `<button type="button" class="mdc-button mdc-snackbar__action">
+              <div class="mdc-button__ripple"></div>
+              <span class="mdc-button__label">${label}</span>
+            </button>`;
+  }
+
+  _dismissButton (icon = "close") {
+    return `<button type="button" class="mdc-snackbar__dismiss mdc-icon-button material-icons">${icon}</button>`;
+  }
+
+  /**
+   * Cleanup the existing snackbar.
+   *
+   * @private
+   */
+  _cleanup () {
+    if (isPresent (this._snackbar)) {
+      // Remove the root element embedded in the snackbar component.
+      this._snackbar.root_.remove ();
+
+      // Destroy the snackbar component.
+      this._snackbar.destroy ();
+      this._snackbar = null;
+    }
+  }
+
+  willOpen () {
+
+  }
+
+  didOpen () {
+
+  }
+
+  willClose () {
+
+  }
+
+  didClose () {
+
+  }
+}
