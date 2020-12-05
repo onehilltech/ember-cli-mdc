1e1db3473bf3cb25fa11a3c3aff40ff3e1d391f6
James Hill <james@onehilltech.com>
2020-12-05 00:02:40 -0500

    chore: prepping for update to 3.18

--- packages/mdc-snackbar/addon/initializers/mdc-snackbar.js
+++ packages/mdc-snackbar/addon/initializers/mdc-snackbar.js
@@ -1,9 +1,6 @@
-import Controller from '@ember/controller';
-import Snackbar from 'ember-cli-mdc-snackbar/mixins/controller/snackbar';
-
-export function initialize (/* application */) {
-  Controller.reopen (Snackbar);
-}
+export function initialize (application) {
+  application.inject ('route', 'snackbar', 'service:snackbar');
+  application.inject ('controller', 'snackbar', 'service:snackbar');}
 
 export default {
   initialize
--- packages/mdc-snackbar/app/instance-initializers/mdc-snackbar.js
+++
@@ -1,1 +0,0 @@
-export { default, initialize } from 'ember-cli-mdc-snackbar/instance-initializers/mdc-snackbar';
