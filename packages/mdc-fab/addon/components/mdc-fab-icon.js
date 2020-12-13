b7a07f77e043fc77304f5a17cd69117b62c39b73
James Hill <james@onehilltech.com>
2020-12-13 15:35:15 -0500

    chore: cleaned up the fab design

--- packages/mdc-fab/addon/components/mdc-fab-icon.js
+++
@@ -1,5 +0,0 @@
-import IconComponent from 'ember-cli-mdc-icon/components/mdc-icon';
-
-export default class MdcFabIcon extends IconComponent {
-
-}
--- packages/mdc-fab/addon/components/mdc-fab-link-to.js
+++ packages/mdc-fab/addon/components/mdc-fab-link-to.js
@@ -1,12 +1,7 @@
 import LinkComponent from '@ember/routing/link-component';
-import { isPresent } from '@ember/utils';
-import { HAS_BLOCK } from '@glimmer/component';
 
-export default class MdcFabLinkComponent extends LinkComponent {
-  classNames = ['mdc-fab'];
-  classNameBindings = ['extended:mdc-fab--extended', 'exited:mdc-fab--exited', 'mini:mdc-fab--mini'];
+export default LinkComponent.extend ({
+  classNames: ['mdc-fab'],
 
-  get extended () {
-    return isPresent (this.label) || this.hasBlock;
-  }
-}
+  classNameBindings: ['label:mdc-fab--extended', 'exited:mdc-fab--exited', 'mini:mdc-fab--mini']
+})
