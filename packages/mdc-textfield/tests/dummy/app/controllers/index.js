a2a3508ef25b3cd97ac1a109cdfdc27020ecef81
James Hill <james@onehilltech.com>
2020-12-18 00:22:37 -0500

    chore: update test demonstrations

--- packages/mdc-textfield/tests/dummy/app/controllers/index.js
+++ packages/mdc-textfield/tests/dummy/app/controllers/index.js
@@ -1,7 +1,11 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class IndexController extends Controller {
+  @tracked
+  validationMessages;
+
   @action
   alert () {
     alert ('Something was clicked');
--- packages/mdc-textfield/tests/dummy/app/routes/index.js
+++ packages/mdc-textfield/tests/dummy/app/routes/index.js
@@ -4,6 +4,12 @@ export default Route.extend({
   setupController (controller) {
     this._super (...arguments);
 
+    controller.validationMessages = {
+      valueMissing: 'You must provide a value here.',
+      rangeUnderflow: 'This number is too small!',
+      rangeOverflow: 'This number is too big!'
+    };
+
     controller.setProperties ({
       requirements: Object.freeze ([
         {description: 'Must be 8 characters or longer', pattern: /[\w\W]{8,}/},
