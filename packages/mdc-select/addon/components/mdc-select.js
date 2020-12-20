3f362885d376dbf6d326ff67e62820b1fd8cdb5f
James Hill <james@onehilltech.com>
2020-12-20 02:06:30 -0500

    chore: added helper text support

--- packages/mdc-select/addon/components/mdc-select.js
+++ packages/mdc-select/addon/components/mdc-select.js
@@ -3,6 +3,8 @@
 import Component from 'ember-cli-mdc-base/component';
 import listener from 'ember-cli-mdc-base/listener';
 
+import { tracked } from '@glimmer/tracking';
+
 import { A } from '@ember/array';
 import { isEmpty, isPresent } from '@ember/utils';
 import { guidFor } from '@ember/object/internals';
@@ -15,6 +17,9 @@ function noOp () { }
 export default class MdcSelectComponent extends Component {
   doPrepareElement (element) {
     const { value: option } = this.args;
+
+    this.labelId = guidFor (this);
+    this.helperId = `${guidFor (this)}__helper-text`;
 
     if (isPresent (option)) {
       // We need to pre-select the option.
@@ -42,6 +47,12 @@ export default class MdcSelectComponent extends Component {
   get isOutlined () {
     return this.args.style === 'outlined';
   }
+
+  @tracked
+  labelId;
+
+  @tracked
+  helperId;
 
   @listener ('MDCSelect:change')
   change (ev) {
@@ -74,16 +85,26 @@ export default class MdcSelectComponent extends Component {
     return A (this.args.options);
   }
 
-  get labelId () {
-    return guidFor (this);
-  }
-
   get leadingIconClick () {
     return this.args.leadingIconClick || noOp;
   }
 
   get trailingIconClick () {
     return this.args.trailingIconClick || noOp;
+  }
+
+  get helperLine () {
+    return isPresent (this.helperText);
+  }
+
+  get helperText () {
+    let { errorMessage, helperText } = this.args;
+    return errorMessage || this.validationMessage || helperText;
+  }
+
+  get persistentHelperText () {
+    let { persistentHelperText, errorMessage } = this.args;
+    return isPresent (errorMessage) || isPresent (this.validationMessage) || persistentHelperText;
   }
 
   /// Adapter attributes
