fd795c96481eeeb8761a7ec285869915d7a4a70a
James Hill <james@onehilltech.com>
2020-12-21 15:48:37 -0500

    chore: saving current state

---
+++ packages/mdc-image-list/blueprints/ember-cli-mdc-image-list/index.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
+const { version } = require ('../../package.json');
+const target = require ('ember-cli-mdc-utils').target (version);
+
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/image-list', target: '^6.0.0'}
+  ],
+
+  addons: [
+    {name: 'ember-cli-mdc-shape', target},
+    {name: 'ember-cli-mdc-theme', target},
+    {name: 'ember-cli-mdc-typography', target}
+  ]
+});
