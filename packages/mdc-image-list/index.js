9792d186837e7eb923016cadf3737767a5b076ab
James Hill <james@onehilltech.com>
2020-12-21 21:10:51 -0500

    chore: added dummy application

--- packages/mdc-image-list/index.js
+++ packages/mdc-image-list/index.js
@@ -3,26 +3,18 @@
 module.exports = {
   name: require('./package').name,
 
-  included (app) {
-    this._super (...arguments);
-
-    app.import ({
-      development: 'node_modules/@material/image-list/dist/mdc.iconButton.js',
-      production: 'node_modules/@material/image-list/dist/mdc.iconButton.min.js'
-    });
-  },
-
   optionsFor (type, options) {
     if (type === 'sass') {
       options.cacheInclude = options.cacheInclude || [];
       options.cacheInclude.push (/addon\.scss/);
       options.cacheInclude.push (/_app-theme\.scss/);
 
-      options.cacheInclude.push (/@material\/animation/);
-      options.cacheInclude.push (/@material\/base/);
-      options.cacheInclude.push (/@material\/icon-button/);
-      options.cacheInclude.push (/@material\/ripple/);
+      options.cacheInclude.push (/@material\/feature-targeting/);
+      options.cacheInclude.push (/@material\/image-list/);
+      options.cacheInclude.push (/@material\/rtl/);
+      options.cacheInclude.push (/@material\/shape/);
       options.cacheInclude.push (/@material\/theme/);
+      options.cacheInclude.push (/@material\/typography/);
     }
 
     return options;
