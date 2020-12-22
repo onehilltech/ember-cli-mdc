e1c83265aa1af468af4a988f0d487997b5ae98c3
James Hill <james@onehilltech.com>
2020-12-22 00:50:42 -0500

    feat: completed implementation of the touch target modifier

---
+++ packages/mdc-touch-target/blueprints/ember-cli-mdc-touch-target/index.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
+const { version } = require ('../../package.json');
+const target = require ('ember-cli-mdc-utils').target (version);
+
+module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/touch-target', target: '^6.0.0'}
+  ],
+
+  addons: [
+    {name: 'ember-cli-mdc-base', target}
+  ]
+});
