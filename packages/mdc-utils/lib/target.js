9b0b012f335d9c5dc8f767c9ba8e1233df6f8cb1
James Hill <james@onehilltech.com>
2020-12-29 01:16:40 -0500

    fix: target failed if version did not contain prerelease

--- packages/mdc-utils/lib/target.js
+++ packages/mdc-utils/lib/target.js
@@ -6,7 +6,7 @@ const semver = require ('semver');
 function target (version) {
   let prerelease = semver.prerelease (version);
 
-  if (prerelease.length !== 0) {
+  if (!!prerelease && prerelease.length !== 0) {
     // Let's update to the latest pre-release version.
     let coerced = semver.coerce (version);
     return `^${coerced}-${prerelease[0]}.0`;
