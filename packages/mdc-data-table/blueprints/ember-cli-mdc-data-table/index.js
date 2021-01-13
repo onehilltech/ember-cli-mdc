00598d96132b8e1ca6f5ad3217d6ca3af0175685
James Hill <james@onehilltech.com>
2021-01-13 08:50:42 -0500

    chore: updated to material 9.0.0, and fixed bug in template

--- packages/mdc-data-table/blueprints/ember-cli-mdc-data-table/index.js
+++ packages/mdc-data-table/blueprints/ember-cli-mdc-data-table/index.js
@@ -4,10 +4,6 @@ const { Blueprint } = require ('ember-cli-blueprint-helpers');
 const { version } = require ('../../package.json');
 
 module.exports = Blueprint.extend ({
-  packages: [
-    {name: '@material/data-table', target: '^6.0.0'}
-  ],
-
   addons: [
     {name: 'ember-cli-mdc-base', target: `^${version}` },
     {name: '@ember/render-modifiers'},
