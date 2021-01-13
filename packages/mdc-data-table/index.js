77d70ed3cc50c8b2c4a2b1c7f5a8ccc16fbbdb3d
James Hill <james@onehilltech.com>
2021-01-13 09:38:23 -0500

    chore: fixed imports

--- packages/mdc-data-table/index.js
+++ packages/mdc-data-table/index.js
@@ -1,15 +1,5 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name,
-
-  included (app) {
-    this._super (...arguments);
-
-    app.import ({
-      development: 'node_modules/@material/data-table/dist/mdc.dataTable.js',
-      production: 'node_modules/@material/data-table/dist/mdc.dataTable.min.js'
-    });
-  },
-
+  name: require('./package').name
 };
