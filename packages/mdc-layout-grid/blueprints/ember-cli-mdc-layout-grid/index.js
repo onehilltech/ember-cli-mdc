50daa0d925dde91ff9415248b53ae6496a6cc4a5
James Hill <james@onehilltech.com>
2021-01-13 10:10:25 -0500

    chore: updated to material 9.0.0

--- packages/mdc-layout-grid/blueprints/ember-cli-mdc-layout-grid/index.js
+++ packages/mdc-layout-grid/blueprints/ember-cli-mdc-layout-grid/index.js
@@ -3,7 +3,5 @@
 const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
 module.exports = Blueprint.extend ({
-  packages: [
-    {name: '@material/layout-grid', target: '^6.0.0'}
-  ]
+
 });
--- packages/mdc-layout-grid/index.js
+++ packages/mdc-layout-grid/index.js
@@ -1,16 +1,5 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name,
-
-  optionsFor (type, options) {
-    if (type === 'sass') {
-      options.cacheInclude = options.cacheInclude || [];
-      options.cacheInclude.push (/addon\.scss/);
-
-      options.cacheInclude.push (/@material\/layout-grid/);
-    }
-
-    return options;
-  }
+  name: require('./package').name
 };
