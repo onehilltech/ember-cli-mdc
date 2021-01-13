3a967ad086c82fa106212148b52e52de2f33e121
James Hill <james@onehilltech.com>
2021-01-13 01:13:28 -0500

    fix: fixed sass issues

--- packages/mdc-shape/blueprints/ember-cli-mdc-shape/index.js
+++ packages/mdc-shape/blueprints/ember-cli-mdc-shape/index.js
@@ -4,11 +4,5 @@ const { Blueprint } = require ('ember-cli-blueprint-helpers');
 const { version } = require ('../../package.json');
 
 module.exports = Blueprint.extend ({
-  packages: [
-    {name: '@material/shape', target: '^9.0.0'}
-  ],
 
-  addons: [
-    {name: 'ember-cli-mdc-sass', target: `^${version}`}
-  ]
 });
