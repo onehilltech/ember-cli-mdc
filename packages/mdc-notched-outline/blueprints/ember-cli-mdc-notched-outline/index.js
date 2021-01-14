5661c3009dd2a8b61a26675739fc647a779d4c97
James Hill <james@onehilltech.com>
2021-01-13 23:11:47 -0500

    chore: updated to material 9.0.0

--- packages/mdc-notched-outline/blueprints/ember-cli-mdc-notched-outline/index.js
+++ packages/mdc-notched-outline/blueprints/ember-cli-mdc-notched-outline/index.js
@@ -4,10 +4,6 @@ const { Blueprint } = require ('ember-cli-blueprint-helpers');
 const { version } = require ('../../package.json');
 
 module.exports = Blueprint.extend ({
-  packages: [
-    {name: '@material/notched-outline', target: '^6.0.0'}
-  ],
-
   addons: [
     { name: 'ember-cli-mdc-floating-label', target: `^${version}` }
   ]
--- packages/mdc-notched-outline/index.js
+++ packages/mdc-notched-outline/index.js
@@ -1,29 +1,5 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name,
-
-  included (app) {
-    this._super (...arguments);
-
-    app.import ({
-      development: 'node_modules/@material/notched-outline/dist/mdc.notchedOutline.js',
-      production: 'node_modules/@material/notched-outline/dist/mdc.notchedOutline.min.js'
-    });
-  },
-
-  optionsFor (type, options) {
-    if (type === 'sass') {
-      options.cacheInclude = options.cacheInclude || [];
-      options.cacheInclude.push (/addon\.scss/);
-      options.cacheInclude.push (/_app-theme\.scss/);
-
-      options.cacheInclude.push (/@material\/animation/);
-      options.cacheInclude.push (/@material\/base/);
-      options.cacheInclude.push (/@material\/notched-outline/);
-      options.cacheInclude.push (/@material\/theme/);
-    }
-
-    return options;
-  }
+  name: require('./package').name
 };
