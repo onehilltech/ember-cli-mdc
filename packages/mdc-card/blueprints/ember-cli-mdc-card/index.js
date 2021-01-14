911e5622f2bd599b8a1c529b442a5f8a193e5235
James Hill <james@onehilltech.com>
2021-01-14 01:01:32 -0500

    chore: updated to material 9.0.0

--- packages/mdc-card/blueprints/ember-cli-mdc-card/index.js
+++ packages/mdc-card/blueprints/ember-cli-mdc-card/index.js
@@ -4,15 +4,10 @@ const { Blueprint } = require ('ember-cli-blueprint-helpers');
 const { version } = require ('../../package.json');
 
 module.exports = Blueprint.extend ({
-  packages: [
-    {name: '@material/card', target: '^6.0.0'}
-  ],
-
   addons: [
     {name: 'ember-cli-mdc-elevation', target: `^${version}`},
     {name: 'ember-cli-mdc-ripple', target: `^${version}`},
     {name: 'ember-cli-mdc-rtl', target: `^${version}`},
-    {name: 'ember-cli-mdc-button', target: `^${version}`},
     {name: 'ember-cli-mdc-icon-button', target: `^${version}`}
   ]
 });
--- packages/mdc-icon-button/blueprints/ember-cli-mdc-icon-button/index.js
+++ packages/mdc-icon-button/blueprints/ember-cli-mdc-icon-button/index.js
@@ -4,6 +4,10 @@ const { Blueprint } = require ('ember-cli-blueprint-helpers');
 const { version } = require ('../../package.json');
 
 module.exports = Blueprint.extend ({
+  packages: [
+    {name: '@material/icon-button', target: '^9.0.0'}
+  ],
+
   addons: [
     {name: 'ember-cli-mdc-button', target: `^${version}`},
     {name: '@ember/render-modifiers'}
