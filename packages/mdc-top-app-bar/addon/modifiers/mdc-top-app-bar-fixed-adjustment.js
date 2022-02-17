89cfbc4b537b856a4dbc4e142ce9c11f70464239
James Hill <james@onehilltech.com>
2022-02-17 08:11:56 -0500

    feat: can dynamically disabled fixed adjustment

--- packages/mdc-top-app-bar/addon/modifiers/mdc-top-app-bar-fixed-adjustment.js
+++ packages/mdc-top-app-bar/addon/modifiers/mdc-top-app-bar-fixed-adjustment.js
@@ -5,24 +5,36 @@ export default class MdcTopAppBarFixedAdjustmentModifier extends Modifier {
   _currentClassName;
 
   didReceiveArguments () {
-    let { dense, prominent, short } = this.args.named;
+    let { dense, prominent, short, disabled = false } = this.args.named;
 
-    if (prominent) {
-      this.applyClassName ('mdc-top-app-bar--prominent-fixed-adjust')
-    }
-    else if (dense) {
+    if (!disabled) {
+      // The user does not want to disabled this adjustment. Apply the correct
+      // adjustment based on the other named properties.
+
       if (prominent) {
-        this.applyClassName ('mdc-top-app-bar--dense-prominent-fixed-adjust');
+        this.applyClassName ('mdc-top-app-bar--prominent-fixed-adjust')
+      }
+      else if (dense) {
+        if (prominent) {
+          this.applyClassName ('mdc-top-app-bar--dense-prominent-fixed-adjust');
+        }
+        else {
+          this.applyClassName ('mdc-top-app-bar--dense-fixed-adjust');
+        }
+      }
+      else if (short) {
+        this.applyClassName ('mdc-top-app-bar--short-fixed-adjust');
       }
       else {
-        this.applyClassName ('mdc-top-app-bar--dense-fixed-adjust');
+        this.applyClassName ('mdc-top-app-bar--fixed-adjust');
       }
     }
-    else if (short) {
-      this.applyClassName ('mdc-top-app-bar--short-fixed-adjust');
-    }
     else {
-      this.applyClassName ('mdc-top-app-bar--fixed-adjust');
+      // We need to remove the fixed adjustment from the element.
+      if (isPresent (this._currentClassName)) {
+        this.element.classList.remove (this._currentClassName);
+        this._currentClassName = null;
+      }
     }
   }
 
