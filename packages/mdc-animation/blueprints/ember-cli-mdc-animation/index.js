ca6e7c3c2ab177c3184414541aa60e3ceb69486c
James Hill <james@onehilltech.com>
2022-05-15 23:14:44 -0400

    chore: update to ember 3.28 and material 14.0

--- packages/mdc-animation/blueprints/ember-cli-mdc-animation/index.js
+++ packages/mdc-animation/blueprints/ember-cli-mdc-animation/index.js
@@ -6,7 +6,7 @@ const target = require ('ember-cli-mdc-utils').target (version);
 
 module.exports = Blueprint.extend ({
   packages: [
-    {name: '@material/animation', target: '^9.0.0'}
+    {name: '@material/animation', target: '^14.0.0'}
   ],
 
   addons: [
