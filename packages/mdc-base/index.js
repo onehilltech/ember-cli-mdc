24e70303784c314168cd30d123cee0f03eb68332
James Hill <james@onehilltech.com>
2022-05-16 07:23:55 -0700

    fix: removed invalid strings

--- packages/mdc-base/index.js
+++ packages/mdc-base/index.js
@@ -1,17 +1,5 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name,
-<<<<<<< HEAD
-
-  included (app) {
-    this._super (...arguments);
-
-    app.import ({
-      development: 'node_modules/@material/base/dist/mdc.base.js',
-      production: 'node_modules/@material/base/dist/mdc.base.min.js'
-    });
-  }
-=======
->>>>>>> df7a18be (v3.18.0...v3.28.5)
+  name: require('./package').name
 };
