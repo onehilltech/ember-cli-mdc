3066161f59992e480b510b8b1349f479b9850bd5
James Hill <james@onehilltech.com>
2022-05-18 22:33:56 -0400

    chore: removed the package from the blueprint

--- packages/mdc-animation/blueprints/ember-cli-mdc-animation/index.js
+++ packages/mdc-animation/blueprints/ember-cli-mdc-animation/index.js
@@ -5,7 +5,5 @@ const { version } = require('../../package.json');
 const target = require('ember-cli-mdc-utils').target(version);
 
 module.exports = Blueprint.extend({
-  packages: [{ name: '@material/animation', target: '^14.0.0' }],
-
   addons: [{ name: 'ember-cli-mdc-sass', target }],
 });
--- packages/mdc-typography/blueprints/ember-cli-mdc-typography/index.js
+++ packages/mdc-typography/blueprints/ember-cli-mdc-typography/index.js
@@ -5,10 +5,6 @@ const { version } = require ('../../package.json');
 const target = require ('ember-cli-mdc-utils').target (version);
 
 module.exports = Blueprint.extend ({
-  packages: [
-    {name: '@material/typography', target: '^14.0.0'}
-  ],
-
   addons: [
     {name: 'ember-cli-mdc-sass', target },
     {name: 'ember-modifier'}
