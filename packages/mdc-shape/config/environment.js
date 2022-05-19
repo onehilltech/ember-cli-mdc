b5c02f8b009cab2757e22187bd7449ee16d72e7b
James Hill <james@onehilltech.com>
2022-05-19 00:18:09 -0400

    chore: removed conflict tags

--- packages/mdc-shape/config/environment.js
+++ packages/mdc-shape/config/environment.js
@@ -1,10 +1,5 @@
 'use strict';
 
-<<<<<<< HEAD
-module.exports = function (/* environment, config */) {
-  return { };
-=======
 module.exports = function (/* environment, appConfig */) {
   return {};
->>>>>>> 7c16754b (v3.18.0...v3.28.5)
 };
