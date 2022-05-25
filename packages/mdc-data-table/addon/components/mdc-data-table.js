cc87c2aa4eb78fb50c6a49d0108cfefabf22fbb5
James Hill <james@onehilltech.com>
2022-05-25 18:10:12 -0400

    fix: layout did not update under certain use cases

--- packages/mdc-data-table/addon/components/mdc-data-table.js
+++ packages/mdc-data-table/addon/components/mdc-data-table.js
@@ -234,7 +234,12 @@ export default class MdcDataTableComponent extends Component {
 
   @action
   updateData () {
-    this.computeTableData ();
+    try {
+      this.computeTableData ();
+    }
+    catch (err) {
+      console.error (err);
+    }
   }
 
   /**
@@ -290,34 +295,44 @@ export default class MdcDataTableComponent extends Component {
     // to update its layout. This will ensure we have the correct checkboxes.
 
     if (refreshLayoutNext) {
-      next (this, 'layout');
+      next (this, 'performLayoutUpdate');
     }
     else {
-      this.layout ();
+      this.performLayoutUpdate ();
     }
   }
 
-  layout () {
+  /**
+   * Update the layout for the component.
+   */
+  performLayoutUpdate () {
     this.component.layout ();
     this.stale = this.rows.length !== this.component.getRows ().length;
 
-    return this.stale;
+    if (this.stale) {
+      next (this, 'performLayoutUpdate');
+    }
   }
 
   @action
   updateSelections () {
-    const rows = this.component.getRows ();
+    try {
+      const rows = this.component.getRows ();
 
-    if (isEmpty (rows)) {
-      return;
+      if (isEmpty (rows)) {
+        return;
+      }
+
+      if (this.stale && !this.layout ()) {
+        return;
+      }
+
+      const ids = this.selected.map (item => this.idForItem (item));
+      this.component.setSelectedRowIds (ids);
     }
-
-    if (this.stale && !this.layout ()) {
-      return;
+    catch (err) {
+      console.log (err);
     }
-
-    const ids = this.selected.map (item => this.idForItem (item));
-    this.component.setSelectedRowIds (ids);
   }
 
   get fields () {
@@ -366,7 +381,7 @@ export default class MdcDataTableComponent extends Component {
   @action
   gotoLastPage () {
     this.pagination.gotoLastPage ();
-    this.computeTableData ( true);
+    this.computeTableData (true);
   }
 
   @action
