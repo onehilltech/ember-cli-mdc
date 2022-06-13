5c128bc048d236112b992e07855b09db4d42830e
James Hill <james@onehilltech.com>
2022-06-13 18:23:45 -0400

    feat: close the speed dial with an action is selected

--- packages/mdc-fab/addon/components/mdc-speed-dial.js
+++ packages/mdc-fab/addon/components/mdc-speed-dial.js
@@ -1,6 +1,9 @@
 import Component from '@glimmer/component';
 import { tracked } from "@glimmer/tracking";
 import { action } from '@ember/object';
+import { isPresent } from '@ember/utils';
+
+import { closest } from '@material/dom/ponyfill';
 
 export default class MdcSpeedDialComponent extends Component {
   @tracked
@@ -9,6 +12,15 @@ export default class MdcSpeedDialComponent extends Component {
   @action
   expand () {
     this.expanded = !this.expanded;
+  }
+
+  @action
+  unexpand (ev) {
+    const fab = closest (ev.target, '.mdc-speed-dial__action > .mdc-fab');
+
+    if (isPresent (fab)) {
+      this.expanded = false;
+    }
   }
 
   get closeIcon () {
