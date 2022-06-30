3863ed520dc4310e567536faea3fc8cf1dd19467
James Hill <james@onehilltech.com>
2022-06-30 11:23:26 -0400

    fix: could not set initial values on radio group

--- packages/mdc-radio/addon/components/mdc-radio-buttons.js
+++ packages/mdc-radio/addon/components/mdc-radio-buttons.js
@@ -1,7 +1,29 @@
-import Component from '@glimmer/component';
+import Component from 'ember-cli-mdc-base/component';
 import { action } from '@ember/object';
+import { isPresent } from '@ember/utils';
 
 export default class MdcRadioButtonsComponent extends Component {
+  @action
+  didInsert (element) {
+    if (isPresent (this.args.value)) {
+      this.select (element, this.args.value);
+    }
+  }
+
+  update (element, [value]) {
+    this.select (element, value);
+  }
+
+  select (element, value) {
+    // Check the button that matches the specified value.
+    const buttons = this.getButtons (element);
+    const initial = buttons.find (button => button.value === `${value}`);
+
+    if (isPresent (initial)) {
+      initial.checked = true;
+    }
+  }
+
   @action
   changed (ev) {
     const { target } = ev;
@@ -11,5 +33,9 @@ export default class MdcRadioButtonsComponent extends Component {
 
   get change () {
     return this.args.change || function () {}
+  }
+
+  getButtons (element) {
+    return Array.from (element.querySelectorAll ('input[type="radio"]'));
   }
 }
