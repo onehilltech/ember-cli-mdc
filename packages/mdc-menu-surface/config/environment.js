92a68936483918cce7822c55d458622bba292174
James Hill <james@onehilltech.com>
2023-03-17 00:58:35 -0400

    chore: updated to 3.28

--- packages/mdc-menu-surface/config/environment.js
+++ packages/mdc-menu-surface/config/environment.js
@@ -1,5 +1,10 @@
 'use strict';
 
+<<<<<<< HEAD
 module.exports = function (/* environment, config */) {
   return { };
+=======
+module.exports = function (/* environment, appConfig */) {
+  return {};
+>>>>>>> 8809389f (v3.18.0...v3.28.6)
 };
