d1c66cf7168e962b561ea60c14dd361a134c9cfa
James Hill <james@onehilltech.com>
2023-03-17 17:20:37 -0400

    chore: added uncommited file

---
+++ packages/mdc-theme/tests/dummy/app/controllers/index.js
@@ -0,0 +1,10 @@
+import Controller from '@ember/controller';
+import { tracked } from "@glimmer/tracking";
+
+export default class IndexController extends Controller {
+  @tracked
+  background;
+
+  @tracked
+  color;
+}
