38d29a45fa3f163210211e9446141bfa6e48462d
James Hill <james@onehilltech.com>
2023-03-17 07:23:21 -0400

    chore: updated to 3.28

--- packages/mdc-slider/README.md
+++ packages/mdc-slider/README.md
@@ -8,8 +8,14 @@ Installation
 
     ember install ember-cli-mdc-slider
 
+<<<<<<< HEAD
 Components
 -------------
+=======
+* Ember.js v3.24 or above
+* Ember CLI v3.24 or above
+* Node.js v12 or above
+>>>>>>> b8fdaabb (v3.18.0...v3.28.6)
 
 This package contains the following top-level components.
 
--- packages/mdc-slider/tests/dummy/app/router.js
+++ packages/mdc-slider/tests/dummy/app/router.js
@@ -1,13 +1,17 @@
 import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import config from 'dummy/config/environment';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
+<<<<<<< HEAD
 Router.map(function() {
   this.route('issues', function() {
     this.route('7');
   });
 });
+=======
+Router.map(function () {});
+>>>>>>> b8fdaabb (v3.18.0...v3.28.6)
