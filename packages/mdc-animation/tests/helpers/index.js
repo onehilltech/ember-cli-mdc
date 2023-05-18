ca574f47b02bba5c0058de8f257558722f59b256
James Hill <hilljh82@gmail.com>
2023-05-17 23:47:20 -0400

    chore: ran codemods

--- packages/mdc-animation/tests/helpers/index.js
+++ packages/mdc-animation/tests/helpers/index.js
@@ -1,9 +1,3 @@
-import {
-  setupApplicationTest as upstreamSetupApplicationTest,
-  setupRenderingTest as upstreamSetupRenderingTest,
-  setupTest as upstreamSetupTest,
-} from 'ember-qunit';
-
 // This file exists to provide wrappers around ember-qunit's / ember-mocha's
 // test setup functions. This way, you can easily extend the setup that is
 // needed per test type.
