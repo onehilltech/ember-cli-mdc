919a098cbafe6ebce0e635fb7eddb52c1c7f61b7
James Hill <hilljh82@gmail.com>
2023-05-17 23:24:32 -0400

    chore: updated to 4.12

--- packages/mdc-touch-target/config/environment.js
+++ packages/mdc-touch-target/config/environment.js
@@ -1,10 +1,5 @@
 'use strict';
 
-<<<<<<< HEAD
 module.exports = function (environment, config) {
-
-=======
-module.exports = function (/* environment, appConfig */) {
   return {};
->>>>>>> 996f5a08 (v3.16.2...v3.28.6)
 };
