361661bab098886ca9a348c60f509dcc5830279a
James Hill <hilljh82@gmail.com>
2023-05-17 23:05:08 -0400

    chore: updated to 4.12

--- packages/mdc-snackbar/index.js
+++ packages/mdc-snackbar/index.js
@@ -2,30 +2,4 @@
 
 module.exports = {
   name: require('./package').name,
-
-  included (app) {
-    this._super (...arguments);
-
-    app.import ({
-      development: 'node_modules/@material/snackbar/dist/mdc.snackbar.js',
-      production: 'node_modules/@material/snackbar/dist/mdc.snackbar.min.js'
-    });
-  },
-
-  optionsFor (type, options) {
-    if (type === 'sass') {
-      options.cacheInclude = options.cacheInclude || [];
-      options.cacheInclude.push (/addon\.scss/);
-      options.cacheInclude.push (/_app-theme\.scss/);
-
-      options.cacheInclude.push (/@material\/animation/);
-      options.cacheInclude.push (/@material\/base/);
-      options.cacheInclude.push (/@material\/rtl/);
-      options.cacheInclude.push (/@material\/snackbar/);
-      options.cacheInclude.push (/@material\/theme/);
-      options.cacheInclude.push (/@material\/typography/);
-    }
-
-    return options;
-  }
 };
