08941302f869f6fa7a02a9aee10dcc9f9b329a4e
James Hill <hilljh82@gmail.com>
2023-05-18 22:26:37 -0400

    chore: fixed runtime and build issues for linear progress

--- packages/mdc-linear-progress/index.js
+++ packages/mdc-linear-progress/index.js
@@ -2,16 +2,4 @@
 
 module.exports = {
   name: require('./package').name,
-<<<<<<< HEAD
-
-  included (app) {
-    this._super (...arguments);
-
-    app.import ({
-      development: 'node_modules/@material/linear-progress/dist/mdc.linearProgress.js',
-      production: 'node_modules/@material/linear-progress/dist/mdc.linearProgress.min.js'
-    });
-  }
-=======
->>>>>>> 520bd251 (v3.18.0...v3.28.6)
 };
