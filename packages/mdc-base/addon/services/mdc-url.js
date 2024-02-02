b6e69f9b06fba380a136288d800d2253ea1b16a7
James Hill <hilljh82@gmail.com>
2024-02-02 12:21:06 -0500

    chore: update the link-to component to use the new mdc-url service

---
+++ packages/mdc-base/addon/services/mdc-url.js
@@ -0,0 +1,45 @@
+import Service, { service } from '@ember/service';
+import { isEmpty, isPresent } from '@ember/utils';
+
+/**
+ * @class MdcUrlService
+ */
+export default class MdcUrlService extends Service {
+  @service
+  router;
+
+  fromComponent (component) {
+    return component.args.href || this.routeUrlFromComponent (component) || '#';
+  }
+
+  routeUrlFromComponent (component) {
+    if (isEmpty (component.args.route)) {
+      return null;
+    }
+
+    let options;
+
+    if (isPresent (component.args.queryParams)) {
+      options = {
+        queryParams: component.args.queryParams
+      };
+    }
+
+    if (isPresent (component.args.model)) {
+      if (isPresent (options)) {
+        return this.router.urlFor (component.args.route, component.args.model, options);
+      }
+      else {
+        return this.router.urlFor (component.args.route, component.args.model);
+      }
+    }
+    else {
+      if (isPresent (options)) {
+        return this.router.urlFor (component.args.route, options);
+      }
+      else {
+        return this.router.urlFor (component.args.route);
+      }
+    }
+  }
+}
--- packages/mdc-bottom-navigation/addon/components/mdc-bottom-navigation-link-to.js
+++ packages/mdc-bottom-navigation/addon/components/mdc-bottom-navigation-link-to.js
@@ -1,22 +1,11 @@
-import LinkComponent from '@ember/routing/link-component';
-const { MDCRipple } = mdc.ripple;
+import Component from 'ember-cli-mdc-base/component';
+import { service } from '@ember/service';
 
-export default class MdcBottomNavigationLinkComponent extends LinkComponent {
-  classNames = ['mdc-bottom-navigation__button', 'mdc-bottom-navigation__link'];
-  classNameBindings = ['active:mdc-bottom-navigation--active', 'label:mdc-bottom-navigation__button--with-label', 'horizontal:mdc-bottom-navigation__button--horizontal'];
-  activeClass = 'mdc-bottom-navigation--active';
+export default class MdcBottomNavigationLinkComponent extends Component {
+  @service
+  mdcUrl;
 
-  didInsertElement () {
-    super.didInsertElement ();
-
-    this._ripple = new MDCRipple (this.element);
-    this._ripple.unbounded = true;
-    this.element.setAttribute ('data-mdc-ripple-is-unbounded', '');
-  }
-
-  willDestroyElement () {
-    super.willDestroyElement ();
-
-    this._ripple.destroy ();
+  get href () {
+    return this.mdcUrl.fromComponent (this);
   }
 }
