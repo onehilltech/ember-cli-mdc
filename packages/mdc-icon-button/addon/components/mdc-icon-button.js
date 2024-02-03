087c516ed85a80ce24738a9a270e28369ce91b89
James Hill <hilljh82@gmail.com>
2024-02-02 21:23:59 -0500

    chore: refactored the design of the icon button link

--- packages/mdc-icon-button/addon/components/mdc-icon-button.js
+++ packages/mdc-icon-button/addon/components/mdc-icon-button.js
@@ -1,5 +1,18 @@
-import MdcIconButtonBase from "../-private/mdc-icon-button-base";
+import Component from 'ember-cli-mdc-base/component';
 
-export default class MdcIconButton extends MdcIconButtonBase {
+import { isPresent } from '@ember/utils';
+import { dasherize } from '@ember/string';
 
+export default class MdcIconButton extends Component {
+  get iconClass () {
+    if (isPresent (this.args.iconClass)) {
+      return this.args.iconClass;
+    }
+    else if (isPresent (this.args.style)) {
+      return `material-icons-${dasherize (this.args.style)}`;
+    }
+    else {
+      return 'material-icons';
+    }
+  }
 };
