e8c6f24dd3db0110080dbe6598d52cfdd9ca568b
James Hill <hilljh82@gmail.com>
2024-02-02 20:23:42 -0500

    chore: updated the list components

--- packages/mdc-list/addon/components/mdc-list-item-link-to.js
+++ packages/mdc-list/addon/components/mdc-list-item-link-to.js
@@ -1,44 +1,19 @@
 import Component from '@glimmer/component';
-import { tracked } from "@glimmer/tracking";
 import { service } from '@ember/service';
-import { isPresent } from "@ember/utils";
 
 export default class MdcListItemLinkTo extends Component {
   get tabindex () {
     return this.args.tabindex || 0;
   }
 
-  get href () {
-    return this.args.href || this.urlFor;
-  }
-
-  get urlFor () {
-    if (isPresent (this.args.model)) {
-      if (isPresent (this.args.queryParams)) {
-        return this.router.urlFor (this.args.route, this.args.model, this.options);
-      }
-      else {
-        return this.router.urlFor (this.args.route, this.args.model);
-      }
-    }
-    else {
-      if (isPresent (this.args.queryParams)) {
-        return this.router.urlFor (this.args.route, this.options);
-      }
-      else {
-        return this.router.urlFor (this.args.route);
-      }
-    }
-  }
-
-  get options () {
-    return { queryParams: this.args.queryParams };
-  }
-
   @service
-  router;
+  mdcUrl;
+
+  get href () {
+    return this.mdcUrl.fromComponent (this);
+  }
 
   get activated () {
-    return this.router.currentURL === this.href;
+    return this.mdcUrl.isActive (this);
   }
 }
