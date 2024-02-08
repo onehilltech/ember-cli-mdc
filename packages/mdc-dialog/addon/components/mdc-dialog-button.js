e63c53304b9976cd2a6890ab1e886629253a18ff
James Hill <hilljh82@gmail.com>
2024-02-07 23:18:52 -0500

    chore: updated to 14 and redesigned how dialogs are defined

--- packages/mdc-dialog/addon/components/mdc-dialog-button.js
+++ packages/mdc-dialog/addon/components/mdc-dialog-button.js
@@ -1,6 +1,22 @@
-import ButtonComponent from 'ember-cli-mdc-button/components/mdc-button';
+import MdcButtonComponent from 'ember-cli-mdc-button/components/mdc-button';
 
-export default class MdcDialogButtonComponent extends ButtonComponent {
-  type = 'mdc-dialog__button'
+export default class MdcDialogButtonComponent extends MdcButtonComponent {
+  doPrepareElement (element) {
+    const {
+      default: isDefaultButton = false,
+      initialFocus = false
+    } = this.args;
+
+    if (isDefaultButton) {
+      element.setAttribute ('data-mdc-dialog-button-default', '');
+    }
+
+    if (initialFocus) {
+      element.setAttribute ('data-mdc-dialog-initial-focus', '');
+    }
+  }
+
+  get kind () {
+    return 'mdc-dialog__button';
+  }
 }
-
--- packages/mdc-dialog/tests/integration/components/mdc-dialog-button-test.js
+++ packages/mdc-dialog/tests/integration/components/mdc-dialog-button-test.js
@@ -1,24 +1,24 @@
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
+import { setupRenderingTest } from 'dummy/tests/helpers';
 import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | mdc-dialog-button', function(hooks) {
+module('Integration | Component | mdc-dialog-button', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{mdc-dialog-button}}`);
+    await render(hbs`<MdcDialogButton />`);
 
     assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
-      {{#mdc-dialog-button}}
+      <MdcDialogButton>
         template block text
-      {{/mdc-dialog-button}}
+      </MdcDialogButton>
     `);
 
     assert.dom(this.element).hasText('template block text');
