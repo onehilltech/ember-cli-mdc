a41ffcd5e05acff863fc10a0c9bcd1d602e2c999
James Hill <hilljh82@gmail.com>
2024-02-08 14:35:48 -0500

    chore: finished updating to 14

--- packages/mdc-textfield/addon/components/mdc-textarea.js
+++ packages/mdc-textfield/addon/components/mdc-textarea.js
@@ -1,66 +1,8 @@
-import Component from 'ember-cli-mdc-base/component';
+import MdcTextFieldComponent from './mdc-textfield';
 
-import { tracked } from '@glimmer/tracking';
-import { isPresent } from '@ember/utils';
-import { guidFor } from '@ember/object/internals';
-import { equal } from '@ember/object/computed';
-import { assert } from '@ember/debug';
-
-import { MDCTextField } from '@material/textfield';
-
-const STYLES = ['filled', 'outlined'];
-
-export default class MdcTextareaComponent extends Component {
-  @tracked
-  labelId;
-
-  @tracked
-  helperId;
-
-  get style () {
-    return this.args.style || this.configurator.style || 'filled';
-  }
-
-  get styleClassName () {
-    const style = this.style;
-
-    assert (`The textarea component supports the following styles: ${STYLES}`, STYLES.includes (style));
-
-    return `mdc-text-field--${style}`;
-  }
-
-  @equal ('style', 'filled')
-  filled;
-
-  @equal ('style', 'outlined')
-  outlined;
-
+export default class MdcTextareaComponent extends MdcTextFieldComponent {
   get resizable () {
     const { resizable = true } = this.args;
     return resizable;
-  }
-
-  doPrepareElement (element) {
-    this.labelId = guidFor (this);
-    this.helperId = `${guidFor (this)}__helper-text`;
-  }
-
-  doCreateComponent (element) {
-    return new MDCTextField (element);
-  }
-
-  get helperLine () {
-    let { characterCount = false } = this.args;
-    return isPresent (this.helperText) || characterCount;
-  }
-
-  get helperText () {
-    let { errorMessage, helperText } = this.args;
-    return errorMessage || helperText;
-  }
-
-  get persistentHelperText () {
-    let { persistentHelperText, errorMessage } = this.args;
-    return isPresent (errorMessage) || persistentHelperText;
   }
 }
