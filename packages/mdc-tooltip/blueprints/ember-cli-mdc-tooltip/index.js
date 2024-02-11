0f1a7d7d496a5a8e8c60bc53a4ea0f6987f11419
James Hill <hilljh82@gmail.com>
2024-02-11 00:50:31 -0500

    chore: added blueprint for package

---
+++ packages/mdc-tooltip/blueprints/ember-cli-mdc-tooltip/index.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
+const { version } = require ('../../package.json');
+const target = require ('ember-cli-mdc-utils').target (version);
+
+module.exports = Blueprint.extend ({
+  addons: [
+    {name: 'ember-cli-mdc-button', target },
+    {name: '@ember/render-modifiers', target: '^2.0.0' }
+  ]
+});
